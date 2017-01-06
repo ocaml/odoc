ceff1bf71c871d1642ece0296f7c01c792fa8894
Thomas Refis <thomas.refis@gmail.com>
2017-01-06 15:53:30 +0000

    fix test...

--- test/ocamlary.ml
+++ test/ocamlary.ml
@@ -314,7 +314,7 @@ type (_,_) full_gadt =
 | Tag : (unit,unit) full_gadt
 | First : 'a -> ('a,unit) full_gadt
 | Second : 'a -> (unit,'a) full_gadt
-| Exist : 'b -> (unit, unit) full_gadt
+| Exist : 'a * 'b -> ('b, unit) full_gadt
 (** Wow! It was a GADT! *)
 
 (** This comment is for [partial_gadt]. *)
@@ -323,6 +323,12 @@ type 'a partial_gadt =
 | OfTag of 'a partial_gadt
 | ExistGadtTag : ('a -> 'b) -> 'a partial_gadt
 (** Wow! It was a mixed GADT! *)
+
+(** This comment is for [record_arg_gadt]. *)
+type _ record_arg_gadt =
+  | With_rec : { foo : int } -> unit record_arg_gadt
+  | With_poly_rec : { bar : 'a. 'a -> 'a } -> ('a -> 'a) record_arg_gadt (** *)
+(** Wow! It was a GADT with record arguments *)
 
 (** This comment is for [alias]. *)
 type alias = variant
@@ -400,7 +406,7 @@ type ('a,'b) full_gadt_alias = ('a,'b) full_gadt =
 | Tag : (unit,unit) full_gadt_alias
 | First : 'a -> ('a,unit) full_gadt_alias
 | Second : 'a -> (unit,'a) full_gadt_alias
-| Exist : 'b -> (unit, unit) full_gadt_alias
+| Exist : 'a * 'b -> ('b, unit) full_gadt_alias
 
 (** This comment is for [partial_gadt_alias]. *)
 type 'a partial_gadt_alias = 'a partial_gadt =
