8323e147c8d6b1e6b809dd963b3dd77576a731e6
Thomas Refis <thomas.refis@gmail.com>
2017-01-25 14:39:36 +0000

    better html deps, necessary for canonical paths

--- lib/odocDepends.ml
+++ lib/odocDepends.ml
@@ -44,18 +44,79 @@ let for_compile_step file =
     | unit_name, Some digest -> { Compile. unit_name; digest } :: acc
   ) ~init:[] cmi_infos.Cmi_format.cmi_crcs
 
-(* CR trefis: for the moment this won't handle forward references properly (in
-   documentation or just using -no-alias-deps).
-   At some point we will want to walk the odoctree and list the root of every
-   unresolved path.
+
+(* TODO: for the moment this won't handle forward references properly (in documentation or
+   just using -no-alias-deps). At some point we will want to walk the odoctree and list
+   the root of every unresolved path (or reference rather?).
    Later. *)
+
+module Hash_set : sig
+  type t
+
+  val create : unit -> t
+
+  val add : t -> string -> unit
+
+  val elements : t -> string list
+end = struct
+  type t = (string, unit) Hashtbl.t
+
+  let add t elt = if Hashtbl.mem t elt then () else Hashtbl.add t elt ()
+
+  let create () = Hashtbl.create 42
+
+  let elements t = Hashtbl.fold (fun s () acc -> s :: acc) t []
+end
+
+module Path = DocOck.Paths.Path
+
+let rec get_root set p =
+  match p with
+  | Path.Forward s -> Hash_set.add set s
+  | Path.Root s -> Hash_set.add set s
+  | Path.Dot (p, _) -> get_root set p
+  | Path.Apply (p, _) -> get_root set p
+  | Path.Resolved r ->
+    (* Not sure if we should not go on here. If [p] is resolved then its root is
+       probably imported. Then again, the path might simply have been imported
+       from another file during expansion. *)
+    ()
+
+class ['a] find_roots set = object(self)
+  inherit ['a] DocOckMaps.identifier
+  inherit ['a] DocOckMaps.reference
+  inherit ['a] DocOckMaps.path as super
+  inherit ['a] DocOckMaps.fragment
+  inherit ['a] DocOckMaps.types
+
+  method root x = x
+
+
+  method! path_resolved :
+    type k. ('a, k) Path.Resolved.t -> ('a, k) Path.Resolved.t =
+    fun x ->
+      begin match x with
+      | Path.Resolved.Canonical (_, p) -> get_root set p
+      | _ -> ()
+      end;
+      super#path_resolved x
+end
+
 let for_html_step input =
   let odoctree = Unit.load input in
+  let deps = Hash_set.create () in
   let open DocOck.Types in
-  List.map (function
-    | Unit.Import.Resolved root ->
-      (* TODO: return the digest as well so we stay consistent. *)
-      Root.Unit.to_string (Root.unit root)
-    | Unit.Import.Unresolved (unit_name, digest_opt) ->
-      unit_name
-  ) odoctree.Unit.imports
+  List.iter odoctree.Unit.imports ~f:(fun import ->
+    let import_name =
+      match import with
+      | Unit.Import.Resolved root ->
+        (* TODO: return the digest as well so we stay consistent. *)
+        Root.Unit.to_string (Root.unit root)
+      | Unit.Import.Unresolved (unit_name, digest_opt) ->
+        unit_name
+    in
+    Hash_set.add deps import_name
+  );
+  let find_roots = new find_roots deps in
+  let _ = find_roots#unit odoctree in
+  Hash_set.elements deps
