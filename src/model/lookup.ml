a7c4e0a577f9ba513b936baa8f2e341474072ec4
Anton Bachin <antonbachin@yahoo.com>
2017-11-15 14:40:11 -0600

    Remove module opens in lookup.ml

--- src/model/lookup.ml
+++ src/model/lookup.ml
@@ -1,9 +1,5 @@
-
-open Model
-open Name_env
-
 class ['a] lookup = object
-  val env = empty
+  val env = Name_env.empty
 
   inherit ['a] Maps.types as super
 
@@ -32,50 +28,50 @@ class ['a] lookup = object
   method fragment_module x = x
 
   method reference_module x =
-    lookup_module env x
+    Name_env.lookup_module env x
   method reference_module_type x =
-    lookup_module_type env x
+    Name_env.lookup_module_type env x
   method reference_type x =
-    lookup_type env x
+    Name_env.lookup_type env x
   method reference_constructor x =
-    lookup_constructor env x
+    Name_env.lookup_constructor env x
   method reference_field x =
-    lookup_field env x
+    Name_env.lookup_field env x
   method reference_extension x =
-    lookup_extension env x
+    Name_env.lookup_extension env x
   method reference_exception x =
-    lookup_exception env x
+    Name_env.lookup_exception env x
   method reference_value x =
-    lookup_value env x
+    Name_env.lookup_value env x
   method reference_class x =
-    lookup_class env x
+    Name_env.lookup_class env x
   method reference_class_type x =
-    lookup_class_type env x
+    Name_env.lookup_class_type env x
   method reference_method x =
-    lookup_method env x
+    Name_env.lookup_method env x
   method reference_instance_variable x =
-    lookup_instance_variable env x
+    Name_env.lookup_instance_variable env x
   method reference_label x =
-    lookup_label env x
+    Name_env.lookup_label env x
   method reference_any x =
-    lookup_element env x
+    Name_env.lookup_element env x
 
   method super_module md = super#module_ md
 
   method! module_ md =
-    let open Module in
-    let env = add_module_decl_items md.type_ env in
+    (* let open Module in *)
+    let env = Name_env.add_module_decl_items md.type_ env in
     let this = {< env = env >} in
       this#super_module md
 
   method super_module_type mty = super#module_type mty
 
   method! module_type mty =
-    let open ModuleType in
+    (* let open ModuleType in *)
     let env =
       match mty.expr with
       | None -> env
-      | Some expr -> add_module_type_expr_items expr env
+      | Some expr -> Name_env.add_module_type_expr_items expr env
     in
     let this = {< env = env >} in
       this#super_module_type mty
@@ -83,11 +79,10 @@ class ['a] lookup = object
   method super_unit unt = super#unit unt
 
   method! unit unt =
-    let open Compilation_unit in
-    let env = add_unit unt env in
+    let env = Name_env.add_unit unt env in
     let env =
       match unt.content with
-      | Module items -> add_signature_items items env
+      | Module items -> Name_env.add_signature_items items env
       | Pack _ -> env
     in
     let this = {< env = env >} in
@@ -96,29 +91,26 @@ class ['a] lookup = object
   method super_page page = super#page page
 
   method! page page =
-    let env = add_page page env in
+    let env = Name_env.add_page page env in
     let this = {< env = env >} in
       this#super_page page
 
   method super_class cl = super#class_ cl
 
   method! class_ cl =
-    let open Class in
-    let env = add_class_decl_items cl.type_ env in
+    let env = Name_env.add_class_decl_items cl.type_ env in
     let this = {< env = env >} in
       this#super_class cl
 
   method super_class_type cltyp = super#class_type cltyp
 
   method! class_type cltyp =
-    let open ClassType in
-    let env = add_class_type_expr_items cltyp.expr env in
+    let env = Name_env.add_class_type_expr_items cltyp.expr env in
     let this = {< env = env >} in
       this#super_class_type cltyp
 
   method! documentation_text_element elt =
     let elt = super#documentation_text_element elt in
-    let open Documentation in
     match elt with
     | Reference (r, None) ->
       let open Paths.Reference in
@@ -126,9 +118,9 @@ class ['a] lookup = object
       begin match r with
       | Element Resolved (Identifier (Paths.Identifier.Label _)
                          | Label _ as rr) ->
-        begin match lookup_section_title env rr with
+        begin match Name_env.lookup_section_title env rr with
         | None -> elt
-        | txt -> Documentation.Reference (r, txt)
+        | txt -> Model.Documentation.Reference (r, txt)
         end
       | _ -> elt
       end
