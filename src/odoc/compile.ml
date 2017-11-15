8b5990efe19423baad976954e54f5ad97aa4ae0c
Anton Bachin <antonbachin@yahoo.com>
2017-11-14 22:39:04 -0600

    Get rid of more module opens

--- src/odoc/compile.ml
+++ src/odoc/compile.ml
@@ -14,31 +14,31 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-open Doc_model
+
 
 let it's_all_the_same ~env ~output input reader =
   let fn = Fs.File.to_string input in
   match reader fn with
-  | Not_an_interface  -> failwith "Not_an_interface"
+  | Doc_model.Not_an_interface  -> failwith "Not_an_interface"
   | Wrong_version  -> failwith "Wrong_version"
   | Corrupted  -> failwith "Corrupted"
   | Not_a_typedtree  -> failwith "Not_a_typedtree"
   | Not_an_implementation  -> failwith "Not_an_implementation"
   | Ok unit ->
-    if not unit.Types.Unit.interface then (
+    if not unit.Doc_model.Types.Unit.interface then (
       Printf.eprintf "WARNING: not processing the \"interface\" file.%s\n%!"
         (if not (Filename.check_suffix fn "cmt") then "" (* ? *)
          else Printf.sprintf " Using %S while you should use the .cmti file" fn)
     );
     let resolve_env = Env.build env (`Unit unit) in
-    let resolved = resolve (Env.resolver resolve_env) unit in
+    let resolved = Doc_model.resolve (Env.resolver resolve_env) unit in
     (* [expand unit] fetches [unit] from [env] to get the expansion of local, previously
        defined, elements. We'd rather it got back the resolved bit so we rebuild an
        environment with the resolved unit.
        Note that this is shitty and once rewritten expand should not fetch the unit it is
        working on. *)
     let expand_env = Env.build env (`Unit resolved) in
-    Unit.save output (expand (Env.expander expand_env) resolved)
+    Unit.save output (Doc_model.expand (Env.expander expand_env) resolved)
 
 let root_of_unit ~package ~hidden unit_name digest =
   let file = Root.Odoc_file.create_unit ~force_hidden:hidden unit_name in
@@ -46,15 +46,15 @@ let root_of_unit ~package ~hidden unit_name digest =
 
 let cmti ~env ~package ~hidden ~output input =
   it's_all_the_same ~env ~output input
-    (read_cmti @@ root_of_unit ~package ~hidden)
+    (Doc_model.read_cmti @@ root_of_unit ~package ~hidden)
 
 let cmt ~env ~package ~hidden ~output input =
   it's_all_the_same ~env ~output input
-    (read_cmt @@ root_of_unit ~package ~hidden)
+    (Doc_model.read_cmt @@ root_of_unit ~package ~hidden)
 
 let cmi ~env ~package ~hidden ~output input =
   it's_all_the_same ~env ~output input
-    (read_cmi @@ root_of_unit ~package ~hidden)
+    (Doc_model.read_cmi @@ root_of_unit ~package ~hidden)
 
 (* TODO: move most of this to doc-ock. *)
 let mld ~env ~package ~output input =
