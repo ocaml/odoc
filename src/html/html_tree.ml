ocaml/odoc

commit 3f16641e45ba8a2ec036edf306efe4013980da3e

Move <nav> into <header>
This is the original change requested in ocaml-doc/odoc#80.

See examples here:

  https://www.w3.org/TR/html5/sections.html#the-header-element

This change also removes unnecessary "keyword" markup inside the <h1>
element.

src/html/html_tree.ml
@@ -262,36 +262,47 @@ let page_creator ?kind ~path content =
       in
       add_dotdot ~n (if !Relative_link.semantic_uris then "" else "index.html")
     in
-    let heading : Html_types.flow5_without_header_footer Html.elt list =
+
+    let kind_text =
       match kind with
-      | Some `Page -> []
-      | _ -> [
-        Html.h1 (
-          Markup.keyword (
-            match kind with
-            | None
-            | Some `Mod -> "Module"
-            | Some `Arg -> "Parameter"
-            | Some `Mty -> "Module type"
-            | Some `Cty -> "Class type"
-            | Some `Class -> "Class"
-            | Some `Page  -> assert false
-          ) :: Html.pcdata " " ::
-          [Markup.module_path (List.tl path)]
-        )
-      ]
+      | None
+      | Some `Mod -> "Module"
+      | Some `Arg -> "Parameter"
+      | Some `Mty -> "Module type"
+      | Some `Cty -> "Class type"
+      | Some `Class -> "Class"
+      | Some `Page -> assert false
     in
-    let article = Html.header heading :: content in
-    let has_parent = List.length path > 1 in
-    if not has_parent then
-      article
-    else
-      Html.nav [
-        Html.a ~a:[Html.a_href up_href] [Html.pcdata "Up"];
-        Html.pcdata " – package ";
-        Html.a ~a:[Html.a_href pkg_href] [Html.pcdata (List.hd path)]
-      ]
-      :: article
+
+    let title_heading =
+      Html.h1 [
+        Html.pcdata (kind_text ^ " ");
+        Markup.module_path (List.tl path);
+      ];
+    in
+
+    let header_content =
+      let has_parent = List.length path > 1 in
+      if has_parent then
+        let nav =
+          Html.nav [
+            Html.a ~a:[Html.a_href up_href] [
+              Html.pcdata "Up"
+            ];
+            Html.pcdata " – package ";
+            Html.a ~a:[Html.a_href pkg_href] [
+              Html.pcdata (List.hd path)
+            ];
+          ]
+        in
+        [nav; title_heading]
+      else
+        [title_heading]
+    in
+
+    let header = Html.header header_content in
+
+    header::content
   in
 
   let html : [ `Html ] Html.elt = Html.html head (Html.body wrapped_content) in

commit ac7dbbb6fc11651ea5560be1dbc38e076ce09f19

Don't try to auto-generate titles for .mld pages
Doing so was actually triggering an assertion failure in odoc, a bug
introduced in 3f16641e45ba8a2ec036edf306efe4013980da3e.

In .mld files, title headings should be written by the author of the
file. By contrast, odoc inserts title headings into pages that it fully
generates itself, such as module doc pages.

src/html/html_tree.ml
@@ -261,20 +261,27 @@ let page_creator ?kind ~path header_docs content =
       add_dotdot ~n (if !Relative_link.semantic_uris then "" else "index.html")
     in
 
-    let kind_text =
+    let title_prefix =
       match kind with
       | None
-      | Some `Mod -> "Module"
-      | Some `Arg -> "Parameter"
-      | Some `Mty -> "Module type"
-      | Some `Cty -> "Class type"
-      | Some `Class -> "Class"
-      | Some `Page -> assert false
+      | Some `Mod -> Some "Module"
+      | Some `Arg -> Some "Parameter"
+      | Some `Mty -> Some "Module type"
+      | Some `Cty -> Some "Class type"
+      | Some `Class -> Some "Class"
+      | Some `Page -> None
     in
 
-    let title_heading =
-      Html.h1
-        [Html.pcdata (kind_text ^ " " ^ (String.concat "." (List.tl path)))];
+    let header_docs =
+      match title_prefix with
+      | None ->
+        header_docs
+      | Some prefix ->
+        let title_heading =
+          Html.h1
+            [Html.pcdata (prefix ^ " " ^ (String.concat "." (List.tl path)))]
+        in
+        title_heading::header_docs
     in
 
     let header_content =
@@ -291,9 +298,9 @@ let page_creator ?kind ~path header_docs content =
             ];
           ]
         in
-        nav::title_heading::header_docs
+        nav::header_docs
       else
-        title_heading::header_docs
+        header_docs
     in
 
     let header = Html.header header_content in