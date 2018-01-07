b84f63d6c92262264609534a0b228b284e6c85a4
Anton Bachin <antonbachin@yahoo.com>
2018-01-07 18:42:03 -0500

    HTML expect tests for functors

--- test/html/cases/functor.mli
+++ test/html/cases/functor.mli
@@ -1,9 +1,17 @@
-module type Arg =
+module type S =
 sig
   type t
 end
 
-module F : functor (Arg : Arg) ->
+module type S1 = S -> S
+
+module F1 : functor (Arg : S) -> S
+
+module F2 : functor (Arg : S) -> (S with type t = Arg.t)
+
+module F3 : functor (Arg : S) ->
 sig
   type t = Arg.t
 end
+
+module F4 (Arg : S) : S
