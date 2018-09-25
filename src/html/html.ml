c9a6043705cefff37087808cb5a7a0b98af574f8
Leandro Ostera <leandro@ostera.io>
2018-09-26 01:20:55 +0200

    Functorize Html_generator and use in OCaml syntax
    Regarding changes to test cases:
    
    All of these are whitespace changes, or accidental fixes, due to the
    functorization. Where before we had `") "`, the functorization forced me
    to remove some of this whitespace. Same goes for other symbols such as
    `") => "` turning into `[ ")", " ", Syn.Type.arrow, " "]`, etc.
    
    -> Class: remove unnecessary space in braces
    -> Functor: remove unnecessary space after splitting arrow
    -> Module: accidentally fix RHS of type-equality
    -> Type: remove unnecessary white space around GADT constructors

--- src/html/html.ml
+++ src/html/html.ml
@@ -1,7 +1,7 @@
 module Html_tree = Html_tree
 module Documentation = Documentation
 module To_html_tree = struct
-  module ML = Html_generator
+  module ML = To_ml_html_tree
   module RE = To_re_html_tree
 end
 module List_targets = List_targets
