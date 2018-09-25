4106f5c7afa0c5aabd2191808cea9a5350136709
Leandro Ostera <leandro@ostera.io>
2018-09-26 01:15:41 +0200

    Prepare for Html_generator functor

--- src/html/html.ml
+++ src/html/html.ml
@@ -1,6 +1,6 @@
 module Html_tree = Html_tree
 module To_html_tree = struct
-  module ML = To_html_tree
+  module ML = Html_generator
   module RE = To_re_html_tree
 end
 module List_targets = List_targets
