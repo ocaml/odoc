0a89aaedf4f2dd92b60241c6324d6dcdc3b407f9
Leandro Ostera <ostera@spotify.com>
2018-10-12 10:28:58 +0200

    Install tidy for esy too

--- test/ci/install-deps.sh
+++ test/ci/install-deps.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
 
+# Install Tidy everywhere
+case $TRAVIS_OS_NAME in
+"windows")
+  add-apt-repository -y ppa:robert7/tidy-html5
+  apt-get update
+  apt-get install tidy
+;;
+
+"linux")
+  sudo add-apt-repository -y ppa:robert7/tidy-html5
+  sudo apt-get update
+  sudo apt-get install tidy
+;;
+
+"osx") brew install tidy-html5 ;;
+esac
+
 if [[ $ESY_BUILD == YES ]]; then
   npm --global install esy@0.3.x
 else
-
   OPAM_RELEASES=https://github.com/ocaml/opam/releases/
   OPAM_VERSION=2.0.0
   OPAM_PKG=opam-${OPAM_VERSION}-x86_64
 
   case $TRAVIS_OS_NAME in
   "windows")
-    add-apt-repository -y ppa:robert7/tidy-html5
-    apt-get update
-    apt-get install tidy
-
     wget ${OPAM_RELEASES}/download/${OPAM_VERSION}/${OPAM_PKG}-linux
     mv ${OPAM_PKG}-linux /usr/local/bin/opam
     chmod a+x /usr/local/bin/opam
   ;;
 
   "linux")
-    sudo add-apt-repository -y ppa:robert7/tidy-html5
-    sudo apt-get update
-    sudo apt-get install tidy
-
     wget ${OPAM_RELEASES}/download/${OPAM_VERSION}/${OPAM_PKG}-linux
     sudo mv ${OPAM_PKG}-linux /usr/local/bin/opam
     sudo chmod a+x /usr/local/bin/opam
   ;;
 
   "osx")
-    brew install tidy-html5
-
     wget ${OPAM_RELEASES}/download/${OPAM_VERSION}/${OPAM_PKG}-darwin
     sudo mv ${OPAM_PKG}-linux /usr/local/bin/opam
     sudo chmod a+x /usr/local/bin/opam
