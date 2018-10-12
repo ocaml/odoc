c812a405b52395016cbac21db335d4e2dbc17eb3
Leandro Ostera <leandro@ostera.io>
2018-10-12 23:27:48 +0200

    Try refering to esy binary directly in windows

--- test/ci/print-versions.sh
+++ test/ci/print-versions.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 if [[ $ESY_BUILD == YES ]]; then
-  $(npm bin --global)/esy --version
+  case $TRAVIS_OS_NAME in
+  "windows")
+    # this is brittle
+    /c/ProgramData/nvs/node/10.12.0/x64/node_modules/esy/_build/default/esy/bin/esyCommand.exe --version
+  ;;
+  *)
+    $(npm bin --global)/esy --version
+  ;;
+  esac
 else
   opam --version
   ocaml -version
