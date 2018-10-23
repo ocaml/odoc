b6c6204f24414fa13c3bbc196d84f18ec31082f6
Leandro Ostera <leandro@ostera.io>
2018-10-23 09:36:12 +0200

    Rely on PATH on windows
    Apparently using nvm now appends the node_modules binaries to the PATH.

--- test/ci/print-versions.sh
+++ test/ci/print-versions.sh
@@ -4,7 +4,7 @@ if [[ $ESY_BUILD == YES ]]; then
   case $TRAVIS_OS_NAME in
   "windows")
     # this is brittle
-    /c/ProgramData/nvs/node/10.12.0/x64/node_modules/esy/_build/default/esy/bin/esyCommand.exe --version
+    esy --version
   ;;
   *)
     $(npm bin --global)/esy --version
