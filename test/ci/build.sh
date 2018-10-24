a444949159fe225d9f2d363c1d22eefd81ce192c
Leandro Ostera <leandro@ostera.io>
2018-10-24 13:46:56 +0200

    Build in windows with full path to esy

--- test/ci/build.sh
+++ test/ci/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+case $TRAVIS_OS_NAME in
+"windows")
+  # this is brittle
+  ESY=/c/ProgramData/nvs/node/10.12.0/x64/node_modules/esy/_build/default/esy/bin/esyCommand.exe
+;;
+*)
+  ESY=$(npm bin --global)/esy
+;;
+esac
+
 if [[ $ESY_BUILD == YES ]]; then
-  esy install --verbose
-  esy build --verbose
-  esy make test
+  $ESY install --verbose
+  $ESY build --verbose
+  $ESY make test
 else
   opam pin add -y --no-action odoc .
   opam install -y --deps-only odoc
