ab866a13cdcad1bf39829dbd96dcd940129f2fcc
Anton Bachin <antonbachin@yahoo.com>
2018-10-27 15:53:51 -0500

    Fix typos in bugs.ml comment
    Related #230.

--- test/html/cases/bugs.ml
+++ test/html/cases/bugs.ml
@@ -5,6 +5,6 @@ let foo (type a) ?(bar : a opt) () = ()
 
 type 'a opt' = int option
 let foo' (type a) ?(bar : a opt') () = ()
-(** Similar to the avobe, but the printed tyoe of [~bar] should be [int], not
+(** Similar to the above, but the printed type of [~bar] should be [int], not
     ['a]. This probably requires fixing in the compiler. See
     {:https://github.com/ocaml/odoc/pull/230#issuecomment-433226807}. *)
