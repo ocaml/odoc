26cd2ce7f9febe3208cd5bdb2f9a674621486cee
Anton Bachin <antonbachin@yahoo.com>
2018-11-20 23:19:59 -0600

    Travis: upgrade to opam 2.0.1

--- test/ci/install-deps.sh
+++ test/ci/install-deps.sh
@@ -7,8 +7,8 @@ sudo apt-get install tidy
 if [[ $ESY_BUILD == YES ]]; then
   npm --global install esy@0.3.x
 else
-  wget https://github.com/ocaml/opam/releases/download/2.0.0/opam-2.0.0-x86_64-linux
-  sudo mv opam-2.0.0-x86_64-linux /usr/local/bin/opam
+  wget https://github.com/ocaml/opam/releases/download/2.0.1/opam-2.0.1-x86_64-linux
+  sudo mv opam-2.0.1-x86_64-linux /usr/local/bin/opam
   sudo chmod a+x /usr/local/bin/opam
 
   opam init -y --compiler=$OCAML --disable-sandboxing
