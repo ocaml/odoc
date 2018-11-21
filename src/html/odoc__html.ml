c6368677c118a050d5da4abc800c15a8431f35af
Jack Feser <jack.feser@gmail.com>
2018-11-20 21:48:53 -0500

    Expose Url module. (#252)

--- src/html/odoc__html.ml
+++ src/html/odoc__html.ml
@@ -5,3 +5,9 @@ module To_html_tree = struct
   module RE = To_re_html_tree
 end
 module List_targets = List_targets
+
+(* Exposed as an unstable public API for third-party packages to "hack" on, see
+
+    https://github.com/ocaml/odoc/pull/252
+    https://github.com/ocaml/odoc/issues/236. *)
+module Url = Url
