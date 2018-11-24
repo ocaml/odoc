d479b849ef989a83b260d1c88c87f66eccf992e1
Anton Bachin <antonbachin@yahoo.com>
2018-11-23 23:44:50 -0600

    Twiddle syntax customization modules

--- src/html/to_ml_html_tree.ml
+++ src/html/to_ml_html_tree.ml
@@ -1,20 +1,19 @@
 module Html = Tyxml.Html
+
 open Utils
 
 module ML = Html_generator.Make (struct
-  module Obj = struct
+  module Obj =
+  struct
     let close_tag_closed = ">"
-
     let close_tag_extendable = ".. >"
-
     let field_separator = "; "
-
     let open_tag_closed = "< "
-
     let open_tag_extendable = "< "
   end
 
-  module Type = struct
+  module Type =
+  struct
     let annotation_separator = " : "
 
     let handle_params name args =
@@ -24,76 +23,69 @@ module ML = Html_generator.Make (struct
 
     let handle_constructor_params = handle_params
     let handle_substitution_params = handle_params
-
     let handle_format_params p = p
-
     let type_def_semicolon = false
-
     let private_keyword = "private"
-
     let parenthesize_constructor = false
 
-    module Variant = struct
+    module Variant =
+    struct
       let parenthesize_params = false
     end
 
-    module Tuple = struct
+    module Tuple =
+    struct
       let element_separator = " * "
-
       let always_parenthesize = false
     end
 
-    module Record = struct
+    module Record =
+    struct
       let field_separator = ";"
     end
 
     let var_prefix = "'"
-
     let any = "_"
+    let arrow = Html.span [Html.entity "#45"; Html.entity "gt"]
 
-    let arrow = Html.span [ Html.entity "#45"; Html.entity "gt" ]
-
-    module Exception = struct
+    module Exception =
+    struct
       let semicolon = false
     end
 
-    module GADT = struct
+    module GADT =
+    struct
       let arrow = arrow
     end
 
-    module External = struct
+    module External =
+    struct
       let semicolon = false
-
       let handle_primitives prims =
         List.map (fun p -> Html.pcdata ("\"" ^ p ^ "\" ")) prims
     end
   end
 
-  module Mod = struct
+  module Mod =
+  struct
     let open_tag = "sig"
-
     let close_tag = "end"
-
     let close_tag_semicolon = false
-
     let include_semicolon = false
-
     let functor_keyword = true
   end
 
-  module Class = struct
+  module Class =
+  struct
     let open_tag = "object"
-
     let close_tag = "end"
   end
 
-  module Value = struct
+  module Value =
+  struct
     let variable_keyword = "val"
-
     let semicolon = false
   end
 end)
 
-let page = ML.Page.page
-
-let compilation_unit = ML.Page.compilation_unit
+include ML.Page
