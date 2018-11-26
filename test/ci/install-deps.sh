6d495474a86e0cd75a1253afe9ceaf5965db972c
Anton Bachin <antonbachin@yahoo.com>
2018-11-25 20:49:30 -0600

    Disable Windows build

--- test/ci/install-deps.sh
+++ test/ci/install-deps.sh
@@ -2,8 +2,6 @@
 
 # Install Tidy everywhere
 case $TRAVIS_OS_NAME in
-"windows") choco install html-tidy make;;
-
 "linux")
   sudo add-apt-repository -y ppa:robert7/tidy-html5
   sudo apt-get update
@@ -25,12 +23,6 @@ else
   OPAM_PKG=opam-${OPAM_VERSION}-x86_64
 
   case $TRAVIS_OS_NAME in
-  "windows")
-    wget ${OPAM_RELEASES}/download/${OPAM_VERSION}/${OPAM_PKG}-linux
-    mv ${OPAM_PKG}-linux /usr/bin/opam
-    chmod a+x /usr/bin/opam
-  ;;
-
   "linux")
     wget ${OPAM_RELEASES}/download/${OPAM_VERSION}/${OPAM_PKG}-linux
     sudo mv ${OPAM_PKG}-linux /usr/local/bin/opam
--- test/ci/print-versions.sh
+++ test/ci/print-versions.sh
@@ -3,15 +3,7 @@
 set -x
 
 if [[ $ESY_BUILD == YES ]]; then
-  case $TRAVIS_OS_NAME in
-  "windows")
-    # this is brittle
-    /c/ProgramData/nvs/node/10.13.0/x64/node_modules/esy/_build/default/esy/bin/esyCommand.exe --version
-  ;;
-  *)
-    $(npm bin --global)/esy --version
-  ;;
-  esac
+  $(npm bin --global)/esy --version
 else
   opam --version
   ocaml -version
