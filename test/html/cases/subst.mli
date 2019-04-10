4d2580bbc4d1c1a88d99f701dd8cec46d95d2540
Geoffrey Reedy <geoff@programmer-monk.net>
2019-04-10 06:49:13 -0600

    Add case that currently doesn't come out right

--- test/html/cases/subst.mli
+++ test/html/cases/subst.mli
@@ -43,4 +43,14 @@ module C : sig
   include R with module T := Map.Make (String)
 end
 
-module H : Hashtbl.HashedType
+module type X = sig
+    include S
+    val doit : 'a t -> unit
+end
+
+(* This case doesn't work properly currently. In the end result the type 'a t
+ * is substituted for doit but not for mentions from inside S and the
+ * definition of the type is still there. It appears that the type is actually
+ * substituted away at first but later expand_include comes along and replaces
+ * the include expansion with one that hasn't been rewritten. *)
+module type X2 = X with type 'a t := 'a list
