46870cefa19f893b4c3365d7412f0c8cb9b0b7f6
Jon Ludlam <jon@recoil.org>
2020-04-20 10:42:59 +0100

    Fix Subst test
    Signed-off-by: Jon Ludlam <jon@recoil.org>

--- src/xref2/test/subst/test.ml
+++ src/xref2/test/subst/test.ml
@@ -1,28 +1,37 @@
-    open Odoc_xref2
+open Odoc_xref2
 open Odoc_xref_test
 
 let filter_map f m =
-    let rec inner = function
-    | x::xs -> begin match f x with | Some x' -> x'::inner xs | None -> inner xs end
+  let rec inner = function
+    | x :: xs -> (
+        match f x with Some x' -> x' :: inner xs | None -> inner xs )
     | [] -> []
-    in inner m 
+  in
+  inner m
 
 let resolve_module_name sg name =
-    let rec check = function
-        | Component.Signature.Module (id, _r, _m) :: _rest when Ident.Name.module_ id = name -> id
-        | _ :: rest -> check rest
-        | [] -> failwith "Unknown"
-    in check sg.Component.Signature.items
+  let rec check = function
+    | Component.Signature.Module (id, _r, _m) :: _rest
+      when Ident.Name.module_ id = name ->
+        id
+    | _ :: rest -> check rest
+    | [] -> failwith "Unknown"
+  in
+  check sg.Component.Signature.items
+
 (* Module substitution test *)
 
 let name = "Module substitution"
-let description = {|
+
+let description =
+  {|
 This test substitutes one module for another. We substitute
 SubTargets in place of SubstituteMe, so the result expected is that
 the equations for t, u and v point to SubTargets rather than SubstituteMe
 |}
 
-let test_data = {|
+let test_data =
+  {|
 
 module SubstituteMe : sig
     type t
@@ -45,30 +54,32 @@ end
 
 |}
 
-
 let module_substitution () =
-    let _, _, sg = Common.model_of_string test_data in
+  let _, _, sg = Common.model_of_string test_data in
 
-    let c = Component.Of_Lang.(signature empty sg) in
+  let c = Component.Of_Lang.(signature empty sg) in
 
-    let subst_idents_mod = resolve_module_name c "SubstituteMe" in
-    let subst_targets_mod = resolve_module_name c "SubTargets" in
+  let subst_idents_mod = resolve_module_name c "SubstituteMe" in
+  let subst_targets_mod = resolve_module_name c "SubTargets" in
 
-    let subst =
-      let target = `Local subst_targets_mod in
-      Subst.add_module subst_idents_mod target Subst.identity
-    in
+  let subst =
+    let target = `Local subst_targets_mod in
+    Subst.add_module subst_idents_mod target Subst.identity
+  in
 
-    let m = Find.module_in_sig c "S" in
+  let m =
+    match Find.module_in_sig c "S" with
+    | Some m -> m
+    | None -> failwith "Error finding module!"
+  in
 
-    let m' = Subst.module_ subst m in
+  let m' = Subst.module_ subst m in
 
-    let open Format in
-    fprintf std_formatter "%s\n%s\n%!" name description;
-    fprintf std_formatter "BEFORE\n======\n%!";
-    fprintf std_formatter "S%a\n\n%!" Component.Fmt.module_ m;
-    fprintf std_formatter "AFTER \n======\n%!";
-    fprintf std_formatter "S%a\n\n%!" Component.Fmt.module_ m'
+  let open Format in
+  fprintf std_formatter "%s\n%s\n%!" name description;
+  fprintf std_formatter "BEFORE\n======\n%!";
+  fprintf std_formatter "S%a\n\n%!" Component.Fmt.module_ m;
+  fprintf std_formatter "AFTER \n======\n%!";
+  fprintf std_formatter "S%a\n\n%!" Component.Fmt.module_ m'
 
-let _ =
-    module_substitution ()
+let _ = module_substitution ()
