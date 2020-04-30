fe854db25648b39ff0a011bdc7e04aa28ec055df
Drup <drupyog@zoho.com>
2020-04-30 15:56:54 +0200

    Factorize target handling.

--- src/odoc/man_page.ml
+++ src/odoc/man_page.ml
@@ -5,6 +5,7 @@ let render _ page = Odoc_manpage.Generator.render page
 let files_of_url url = Odoc_manpage.Link.files_of_url url
 
 let renderer = {Renderer.
+  name = "man" ;
   render ;
   files_of_url ;
 }
