81a613d35a3ac0b5b4f228a44d78557c6542b9b9
Jules Aguillon <juloo.dsi@gmail.com>
2020-05-13 18:47:51 +0200

    Don't define ResultMonad.t and OptionMonad.t
    These modules are meant to be opened, and are not defining something new.
    The type 't' would clash most of the type.

--- src/xref2/utils.ml
+++ src/xref2/utils.ml
@@ -1,7 +1,7 @@
 (** The [result] type and a bind operator. This module is meant to be opened. *)
 module ResultMonad = struct
   (** Re-export for compat *)
-  type ('a, 'b) t = ('a, 'b) Result.result = Ok of 'a | Error of 'b
+  type ('a, 'b) result = ('a, 'b) Result.result = Ok of 'a | Error of 'b
 
   let map_error f = function Ok _ as ok -> ok | Error e -> Error (f e)
 
@@ -14,8 +14,6 @@ end
 
 (** A bind operator for the [option] type. This module is meant to be opened. *)
 module OptionMonad = struct
-  type 'a t = 'a option = None | Some of 'a
-
   (* The error case become [None], the error value is ignored. *)
   let of_result = function Result.Ok x -> Some x | Error _ -> None
 
