91560c3390dfa5ea7ea72f0f7526e7839b9396e0
Jon Ludlam <jon@recoil.org>
2020-06-08 15:08:07 +0100

    Add errors module for tools errors
    Signed-off-by: Jon Ludlam <jon@recoil.org>

---
+++ src/xref2/errors.mli
@@ -0,0 +1,44 @@
+(** Errors raised by Tools *)
+
+open Odoc_model.Paths
+
+type handle_subs_error =
+    [ `UnresolvedPath of [ `Module of Cpath.module_ ] (** Failed to resolve a module path when applying a fragment item *)]
+
+type signature_of_module_error =
+  [ `OpaqueModule (** The module does not have an expansion *)
+  | `UnresolvedForwardPath (** The module signature depends upon a forward path *)
+  | `UnresolvedPath of 
+    [ `Module of Cpath.module_ | `ModuleType of Cpath.module_type ]
+    (** The path to the module or module type could not be resolved *) ]
+
+type simple_module_lookup_error =
+  [ `Local of Env.t * Ident.module_ (** Internal error: Found local path during lookup *)
+  | `Unresolved_apply (** [`Apply] argument is not [`Resolved] *)
+  | `Find_failure (*** Internal error: the module was not found in the parent signature *)
+  | `Lookup_failure of Identifier.Module.t (** Could not find the module in the environment *) ]
+
+and simple_module_type_expr_of_module_error =
+  [ `ApplyNotFunctor (** Internal error: attempt made to apply a module that's not a functor *)
+  | `OpaqueModule (** The module does not have an expansion *)
+  | `UnresolvedForwardPath (** The module signature depends upon a forward path *)
+  | handle_subs_error ]
+
+and simple_module_type_lookup_error =
+  [ `LocalMT of Env.t * Cpath.Resolved.module_type (** Internal error: Found local path during lookup *)
+  | `Find_failure (** Internal error: the module was not found in the parent signature *)
+  | `Lookup_failureMT of Identifier.ModuleType.t (** Could not find the module in the environment *) ]
+
+and simple_type_lookup_error =
+  [ `LocalType of Env.t * Cpath.Resolved.type_ (** Internal error: Found local path during lookup *)
+  | `Find_failure (** Internal error: the type was not found in the parent signature *)
+  | `Lookup_failure of Identifier.Path.Type.t (** Could not find the module in the environment *) ]
+
+and parent_lookup_error =
+  [ `Parent_sig of signature_of_module_error (** Error found while calculating the parent signature *)
+  | `Parent_module_type of simple_module_type_lookup_error (** Error found while looking up parent module type *)
+  | `Parent_expr of simple_module_type_expr_of_module_error (** Error found while evaluating parent module expression *)
+  | `Parent_module of simple_module_lookup_error (** Error found while looking up parent module *)
+  | `Fragment_root (* Encountered unexpected fragment root *)
+  ]
+
