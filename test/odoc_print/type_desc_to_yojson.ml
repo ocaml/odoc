a25346a24a30a005327d321a8b0263fc691db667
Jules Aguillon <juloo.dsi@gmail.com>
2020-10-13 18:17:59 +0200

    odoc_print: Encode variants as objects
    Constructors without argument are a string.

--- test/odoc_print/type_desc_to_yojson.ml
+++ test/odoc_print/type_desc_to_yojson.ml
@@ -10,12 +10,10 @@ let rec to_yojson : type a. a t -> a -> yojson =
   | Record fields ->
       let field_to_yojson (F (name, get, t)) = (name, to_yojson t (get a)) in
       `Assoc (List.map field_to_yojson fields)
-  | Variant get ->
-      let case = function
-        | C0 name -> [ `String name ]
-        | C (name, a', t) -> [ `String name; to_yojson t a' ]
-      in
-      `List (case (get a))
+  | Variant get -> (
+      match get a with
+      | C0 name -> `String name
+      | C (name, a', t) -> `Assoc [ name, to_yojson t a' ] )
   | Pair (t1, t2) ->
       let a1, a2 = a in
       `List [ to_yojson t1 a1; to_yojson t2 a2 ]
@@ -23,8 +21,9 @@ let rec to_yojson : type a. a t -> a -> yojson =
       let a1, a2, a3 = a in
       `List [ to_yojson t1 a1; to_yojson t2 a2; to_yojson t3 a3 ]
   | List t -> `List (List.map (to_yojson t) a)
-  | Option t ->
-      let opt = match a with Some a' -> [ to_yojson t a' ] | None -> [] in
-      `List opt
+  | Option t -> (
+      match a with
+      | Some a' -> `Assoc [ "Some", to_yojson t a' ]
+      | None -> `String "None" )
   | To_string to_string -> `String (to_string a)
   | Indirect (f, t) -> to_yojson t (f a)
