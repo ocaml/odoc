793bde8589b22369683654a0e2f6a5973da48053
Jules Aguillon <juloo.dsi@gmail.com>
2020-12-09 18:36:24 +0100

    Compat with 4.02.3
    Documentation on polymorphic variants are not supported.

--- src/xref2/errors.ml
+++ src/xref2/errors.ml
@@ -5,72 +5,84 @@ module Tools_error = struct
   (** Errors raised by Tools *)
 
   type handle_subs_error =
-    [ `UnresolvedPath of [ `Module of Cpath.module_ ]
-      (** Failed to resolve a module path when applying a fragment item *) ]
+    [ `UnresolvedPath of
+      [ `Module of Cpath.module_ ]
+      (* Failed to resolve a module path when applying a fragment item *) ]
 
   type signature_of_module_error =
-    [ `OpaqueModule  (** The module does not have an expansion *)
+    [ `OpaqueModule (* The module does not have an expansion *)
     | `UnresolvedForwardPath
-      (** The module signature depends upon a forward path *)
+      (* The module signature depends upon a forward path *)
     | `UnresolvedPath of
       [ `Module of Cpath.module_ * simple_module_lookup_error
       | `ModuleType of Cpath.module_type * simple_module_type_lookup_error ]
-      (** The path to the module or module type could not be resolved *)
-    | `UnexpandedTypeOf of Component.ModuleType.type_of_desc
-      (** The `module type of` expression could not be expanded *) ]
+      (* The path to the module or module type could not be resolved *)
+    | `UnexpandedTypeOf of
+      Component.ModuleType.type_of_desc
+      (* The `module type of` expression could not be expanded *) ]
 
   and simple_module_lookup_error =
-    [ `Local of Env.t * Ident.path_module
-      (** Internal error: Found local path during lookup *)
-    | `Unresolved_apply  (** [`Apply] argument is not [`Resolved] *)
+    [ `Local of
+      Env.t * Ident.path_module
+      (* Internal error: Found local path during lookup *)
+    | `Unresolved_apply (* [`Apply] argument is not [`Resolved] *)
     | `Find_failure
-    | (*** Internal error: the module was not found in the parent signature *)
+    | (* Internal error: the module was not found in the parent signature *)
       `Lookup_failure of
       Identifier.Path.Module.t
-      (** Could not find the module in the environment *)
-    | `Lookup_failure_root of string  (** Could not find the root module *)
+      (* Could not find the module in the environment *)
+    | `Lookup_failure_root of string (* Could not find the root module *)
     | `Parent of parent_lookup_error ]
 
   and simple_module_type_expr_of_module_error =
     [ `ApplyNotFunctor
-      (** Internal error: attempt made to apply a module that's not a functor *)
-    | `OpaqueModule  (** The module does not have an expansion *)
+      (* Internal error: attempt made to apply a module that's not a functor *)
+    | `OpaqueModule (* The module does not have an expansion *)
     | `UnresolvedForwardPath
-      (** The module signature depends upon a forward path *)
+      (* The module signature depends upon a forward path *)
     | `UnresolvedPath of
       [ `Module of Cpath.module_ * simple_module_lookup_error
       | `ModuleType of Cpath.module_type * simple_module_type_lookup_error ]
     | `Parent of parent_lookup_error ]
 
   and simple_module_type_lookup_error =
-    [ `LocalMT of Env.t * Ident.module_type
-      (** Internal error: Found local path during lookup *)
+    [ `LocalMT of
+      Env.t * Ident.module_type
+      (* Internal error: Found local path during lookup *)
     | `Find_failure
-      (** Internal error: the module was not found in the parent signature *)
-    | `Lookup_failureMT of Identifier.ModuleType.t
-      (** Could not find the module in the environment *)
+      (* Internal error: the module was not found in the parent signature *)
+    | `Lookup_failureMT of
+      Identifier.ModuleType.t
+      (* Could not find the module in the environment *)
     | `Parent of parent_lookup_error ]
 
   and simple_type_lookup_error =
-    [ `LocalType of Env.t * Ident.path_type
-      (** Internal error: Found local path during lookup *)
+    [ `LocalType of
+      Env.t * Ident.path_type
+      (* Internal error: Found local path during lookup *)
     | `Class_replaced
-      (** Class was replaced with a destructive substitution and we're not sure what to do now *)
+      (* Class was replaced with a destructive substitution and we're not sure
+          what to do now *)
     | `Find_failure
-      (** Internal error: the type was not found in the parent signature *)
-    | `Lookup_failureT of Identifier.Path.Type.t
-      (** Could not find the module in the environment *)
+      (* Internal error: the type was not found in the parent signature *)
+    | `Lookup_failureT of
+      Identifier.Path.Type.t
+      (* Could not find the module in the environment *)
     | `Parent of parent_lookup_error ]
 
   and parent_lookup_error =
-    [ `Parent_sig of signature_of_module_error
-      (** Error found while calculating the parent signature *)
-    | `Parent_module_type of simple_module_type_lookup_error
-      (** Error found while looking up parent module type *)
-    | `Parent_expr of simple_module_type_expr_of_module_error
-      (** Error found while evaluating parent module expression *)
-    | `Parent_module of simple_module_lookup_error
-      (** Error found while looking up parent module *)
+    [ `Parent_sig of
+      signature_of_module_error
+      (* Error found while calculating the parent signature *)
+    | `Parent_module_type of
+      simple_module_type_lookup_error
+      (* Error found while looking up parent module type *)
+    | `Parent_expr of
+      simple_module_type_expr_of_module_error
+      (* Error found while evaluating parent module expression *)
+    | `Parent_module of
+      simple_module_lookup_error
+      (* Error found while looking up parent module *)
     | `Fragment_root (* Encountered unexpected fragment root *)
     | `Parent of parent_lookup_error ]
 
@@ -88,11 +100,13 @@ module Tools_error = struct
     | `OpaqueModule -> Format.fprintf fmt "OpaqueModule"
     | `UnresolvedForwardPath -> Format.fprintf fmt "Unresolved forward path"
     | `UnresolvedPath (`Module (p, e)) ->
-        Format.fprintf fmt "Unresolved module path %a (%a)" Component.Fmt.module_path
-          p pp (e :> any)
+        Format.fprintf fmt "Unresolved module path %a (%a)"
+          Component.Fmt.module_path p pp
+          (e :> any)
     | `UnresolvedPath (`ModuleType (p, e)) ->
         Format.fprintf fmt "Unresolved module type path %a (%a)"
-          Component.Fmt.module_type_path p pp (e :> any)
+          Component.Fmt.module_type_path p pp
+          (e :> any)
     | `LocalMT (_, id) -> Format.fprintf fmt "Local id found: %a" Ident.fmt id
     | `Local (_, id) -> Format.fprintf fmt "Local id found: %a" Ident.fmt id
     | `LocalType (_, id) -> Format.fprintf fmt "Local id found: %a" Ident.fmt id
