6e54a987bb31e501336605777fd42a1769ddf924
Jon Ludlam <jon@recoil.org>
2021-01-23 10:11:15 +0000

    Unresolve opaque module paths on functor application
    Modules and module paths that were previously opaque may not be when
    functors are applied - so we explicitly 'unresolve' any path containing
    an opaque path constructor.
    
    Signed-off-by: Jon Ludlam <jon@recoil.org>

--- src/xref2/component.mli
+++ src/xref2/component.mli
@@ -388,6 +388,7 @@ and Substitution : sig
     type_replacement : (TypeExpr.t * TypeDecl.Equation.t) PathTypeMap.t;
     path_invalidating_modules : Ident.path_module list;
     module_type_of_invalidating_modules : Ident.path_module list;
+    unresolve_opaque_paths : bool;
   }
 end
 
