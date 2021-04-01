f19caea21dd428e046eba09489331ad5f287c01f
Jon Ludlam <jon@recoil.org>
2021-04-01 12:05:17 +0100

    Update tests

--- test/xref2/canonical_unit.t/test.ml
+++ test/xref2/canonical_unit.t/test.ml
@@ -1,20 +1,26 @@
 (** Main module of this test. *)
 
-module X = Test__x
+module X = Test_x
 
 (** An other example that is not an unit for comparison.
     @canonical Test.Y *)
-module Test__y = struct
+module Test_y = struct
   type t
 end
 
-module Y = Test__y
+module Y = Test_y
 
 (** An example with the tag inside the sig. *)
-module Test__z = struct
+module Test_z = struct
   (** @canonical Test.Z *)
 
   type t
 end
 
-module Z = Test__z
+module Z = Test_z
+
+
+type t = Test_x.t
+type u = Test_y.t
+type v = Test_z.t
+
