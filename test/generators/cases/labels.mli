11ef243a409948f3177b749fa1a651d670b75fac
lubegasimon <lubegasimon73@gmail.com>
2021-05-07 22:02:45 +0300

    add test cases
    Signed-off-by: lubegasimon <lubegasimon73@gmail.com>

---
+++ test/generators/cases/labels.mli
@@ -0,0 +1,58 @@
+(** {1:L1 Attached to unit} *)
+
+(** {1:L2 Attached to nothing} *)
+
+(** {1:L3 Attached to module} *)
+module A : sig end
+
+(** {1:L4 Attached to type} *)
+type t
+
+(** {1:L5 Attached to value} *)
+val f : t
+
+(** {1:L5bis Attached to external} *)
+external e : unit -> t = "t"
+
+(** {1:L6 Attached to module type} *)
+module type S = sig end
+
+(** {1:L6 Attached to class} *)
+class c : object end
+
+(** {1:L7 Attached to class type} *)
+class type cs = object end
+
+(** {1:L8 Attached to exception} *)
+exception E
+
+type x = ..
+
+(** {1:L9 Attached to extension} *)
+type x += X
+
+(** {1:L10 Attached to module subst} *)
+module S := A
+
+(** {1:L11 Attached to type subst} *)
+type s := t
+
+type u = A' (** {1:L12 Attached to constructor} *)
+
+type v = { f : t (** {1:L13 Attached to field} *) }
+
+(** Testing that labels can be referenced
+    - {!L1}
+    - {!L2}
+    - {!L3}
+    - {!L4}
+    - {!L5}
+    - {!L6}
+    - {!L7}
+    - {!L8}
+    - {!L9}
+    - {!L10}
+    - {!L11}
+    - {!L12}
+    - {!L13}
+  *)
