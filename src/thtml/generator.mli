7dec59ed822de03dd54dafe1a5c2e51ee63619de
Jon Ludlam <jon@recoil.org>
2021-05-12 00:34:07 +0100

    Revert "Add thtml backend"
    This reverts commit 8e7543cd071571f6052582928bb867385e419648.

--- src/thtml/generator.mli
+++
@@ -1,8 +0,0 @@
-open Odoc_document
-
-val render : ?theme_uri:Tree.uri -> indent:bool -> Types.Page.t -> Renderer.page
-
-val doc :
-  xref_base_uri:string ->
-  Types.Block.t ->
-  Html_types.flow5_without_sectioning_heading_header_footer Tyxml.Html.elt list
--- src/thtml/odoc_thtml.ml
+++
@@ -1,5 +0,0 @@
-module Tree = Tree
-(** @canonical Odoc_html.Tree *)
-
-module Generator = Generator
-module Link = Link
