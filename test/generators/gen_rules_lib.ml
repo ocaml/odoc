e5aa10e14acf538a9977b560bc4b932fa53bacc4
Jules Aguillon <juloo.dsi@gmail.com>
2021-07-21 19:09:46 +0200

    Remove some duplicated code in generators tests
    - Factorize the code generating dune rules
    - Avoid reading .targets files several time
    - Simplify some path logic

--- test/generators/gen_rules_lib.ml
+++ test/generators/gen_rules_lib.ml
@@ -10,154 +10,98 @@ type test_case = {
   enabledif : enabledif option;
 }
 
-let render_enabledif = function
-  | Some (Min v) ->
+module Dune = struct
+  let arg_fpath f = Fpath.to_string f
+
+  let arg_dep f = "%{dep:" ^ Fpath.to_string f ^ "}"
+
+  let arg_list args = List.map (fun x -> Atom x) args
+
+  let render_enabledif = function
+    | Some (Min v) ->
+        [
+          List
+            [
+              Atom "enabled_if";
+              List [ Atom ">="; Atom "%{ocaml_version}"; Atom v ];
+            ];
+        ]
+    | Some (Max v) ->
+        [
+          List
+            [
+              Atom "enabled_if";
+              List [ Atom "<="; Atom "%{ocaml_version}"; Atom v ];
+            ];
+        ]
+    | Some (MinMax (min, max)) ->
+        [
+          List
+            [
+              Atom "enabled_if";
+              List
+                [
+                  Atom "and";
+                  List [ Atom ">="; Atom "%{ocaml_version}"; Atom min ];
+                  List [ Atom "<="; Atom "%{ocaml_version}"; Atom max ];
+                ];
+            ];
+        ]
+    | None -> []
+
+  let run cmd = List (Atom "run" :: arg_list cmd)
+
+  let action x = List [ Atom "action"; x ]
+
+  let rule ?enabledif fields =
+    List (Atom "rule" :: fields @ render_enabledif enabledif)
+
+  let simple_rule ?enabledif target cmd =
+    rule ?enabledif
+      [ List [ Atom "target"; Atom (arg_fpath target) ]; action (run cmd) ]
+
+  let rule_with_output_to ?enabledif target cmd =
+    rule ?enabledif
       [
-        List
-          [
-            Atom "enabled_if";
-            List [ Atom ">="; Atom "%{ocaml_version}"; Atom v ];
-          ];
+        action
+          (List [ Atom "with-outputs-to"; Atom (arg_fpath target); run cmd ]);
       ]
-  | Some (Max v) ->
+
+  let runtest_diff ?enabledif file_a file_b =
+    rule ?enabledif
       [
-        List
-          [
-            Atom "enabled_if";
-            List [ Atom "<="; Atom "%{ocaml_version}"; Atom v ];
-          ];
+        List [ Atom "alias"; Atom "runtest" ];
+        action
+          (List
+             [ Atom "diff"; Atom (arg_fpath file_a); Atom (arg_fpath file_b) ]);
       ]
-  | Some (MinMax (min, max)) ->
-      [
-        List
-          [
-            Atom "enabled_if";
-            List
-              [
-                Atom "and";
-                List [ Atom ">="; Atom "%{ocaml_version}"; Atom min ];
-                List [ Atom "<="; Atom "%{ocaml_version}"; Atom max ];
-              ];
-          ];
-      ]
-  | None -> []
 
-let cu_target_rule enabledif dep_path target_path =
-  List
-    ([
-       Atom "rule";
-       List [ Atom "target"; Atom (Fpath.to_string target_path) ];
-       List [ Atom "deps"; Atom (Fpath.to_string dep_path) ];
-       List
-         [
-           Atom "action";
-           List
-             [
-               Atom "run";
-               Atom "ocamlc";
-               Atom "-c";
-               Atom "-bin-annot";
-               Atom "-o";
-               Atom "%{target}";
-               Atom "%{deps}";
-             ];
-         ];
-     ]
-    @ enabledif)
+  let subdir dir rules = List (Atom "subdir" :: Atom (arg_fpath dir) :: rules)
+end
 
-let odoc_target_rule enabledif dep_path target_path =
-  List
-    ([
-       Atom "rule";
-       List [ Atom "target"; Atom (Fpath.basename target_path) ];
-       List [ Atom "deps"; Atom (Fpath.basename dep_path) ];
-       List
-         [
-           Atom "action";
-           List
-             [
-               Atom "run";
-               Atom "odoc";
-               Atom "compile";
-               Atom "-o";
-               Atom "%{target}";
-               Atom "%{deps}";
-             ];
-         ];
-     ]
-    @ enabledif)
+let cu_target_rule enabledif dep target =
+  Dune.simple_rule ?enabledif target
+    [ "ocamlc"; "-c"; "-bin-annot"; "-o"; "%{target}"; Dune.arg_dep dep ]
 
-let odocl_target_rule enabledif dep_path target_path =
-  List
-    ([
-       Atom "rule";
-       List [ Atom "target"; Atom (Fpath.basename target_path) ];
-       List [ Atom "deps"; Atom (Fpath.basename dep_path) ];
-       List
-         [
-           Atom "action";
-           List
-             [
-               Atom "run";
-               Atom "odoc";
-               Atom "link";
-               Atom "-o";
-               Atom "%{target}";
-               Atom "%{deps}";
-             ];
-         ];
-     ]
-    @ enabledif)
+let odoc_target_rule enabledif dep target =
+  Dune.simple_rule ?enabledif target
+    [ "odoc"; "compile"; "-o"; "%{target}"; Dune.arg_dep dep ]
 
-let mld_odoc_target_rule enabledif dep_path target_path =
-  List
-    ([
-       Atom "rule";
-       List [ Atom "target"; Atom (Fpath.basename target_path) ];
-       List [ Atom "deps"; Atom (Fpath.to_string dep_path) ];
-       List
-         [
-           Atom "action";
-           List
-             [
-               Atom "run";
-               Atom "odoc";
-               Atom "compile";
-               Atom "-o";
-               Atom "%{target}";
-               Atom "%{deps}";
-             ];
-         ];
-     ]
-    @ enabledif)
+let odocl_target_rule enabledif dep target =
+  Dune.simple_rule ?enabledif target
+    [ "odoc"; "link"; "-o"; "%{target}"; Dune.arg_dep dep ]
 
-let file_rule { input; odoc; odocl; enabledif; _ } cmt =
-  let enabledif = render_enabledif enabledif in
-  [
-    cu_target_rule enabledif input cmt;
-    odoc_target_rule enabledif cmt odoc;
-    odocl_target_rule enabledif odoc odocl;
-  ]
-
-let mld_file_rule { input; odoc; odocl; enabledif; _ } =
-  let enabledif = render_enabledif enabledif in
-  [
-    mld_odoc_target_rule enabledif input odoc;
-    odocl_target_rule enabledif odoc odocl;
-  ]
-
-let path' () f = Filename.quote (Fpath.to_string f)
-
-let ext' () f = Filename.quote (Fpath.get_ext f)
-
-let gen_rule_for_source_file case =
-  match case.cmt with
-  | Some cmt -> file_rule case cmt
-  | None -> mld_file_rule case
-
-let odocls backend p =
-  let path = Fpath.relativize ~root:backend p in
-  match path with Some p -> p | None -> assert false
+let gen_rule_for_source_file { input; cmt; odoc; odocl; enabledif } =
+  let cmt_rule, input =
+    match cmt with
+    | Some cmt -> ([ cu_target_rule enabledif input cmt ], cmt)
+    | None -> ([], input)
+  in
+  cmt_rule
+  @ [
+      odoc_target_rule enabledif input odoc;
+      odocl_target_rule enabledif odoc odocl;
+    ]
 
 let read_lines ic =
   let lines = ref [] in
@@ -174,154 +118,64 @@ let lines_of_file path =
   close_in ic;
   lines
 
-let create_targets_file f = Fpath.(base f |> set_ext ".targets")
-
-let get_path_to_targets_file backend f =
-  create_targets_file f |> Fpath.to_string |> Fpath.( / ) backend
+let targets_file_path f = Fpath.(base f |> set_ext ".targets")
 
 let expected_targets backend test_case =
-  let targets_file = get_path_to_targets_file backend test_case in
-  try lines_of_file targets_file with _ -> []
+  let targets_file = Fpath.( // ) backend (targets_file_path test_case) in
+  try lines_of_file targets_file |> List.map Fpath.v with _ -> []
 
-let expected_targets' backend test_case =
-  expected_targets backend test_case |> List.map (fun t -> Atom (t ^ ".gen"))
+let gen_targets_file enabledif ?flat_flag backend target_path relinput =
+  let flat_flag = match flat_flag with None -> [] | Some x -> [ x ] in
+  let gen_path = Fpath.add_ext ".gen" target_path in
+  [
+    Dune.subdir backend
+      [
+        Dune.rule_with_output_to ?enabledif gen_path
+          ([
+             "odoc";
+             Fpath.to_string backend ^ "-targets";
+             "-o";
+             ".";
+             Dune.arg_dep relinput;
+           ]
+          @ flat_flag);
+        Dune.runtest_diff ?enabledif target_path gen_path;
+      ];
+  ]
 
-let gen_targets_file enabledif ?flat_flag backend target_path path =
-  let flat_flag = match flat_flag with None -> [] | Some x -> [ Atom x ] in
-  List
-    [
-      Atom "subdir";
-      Atom (Fpath.to_string backend);
-      List
-        ([
-           Atom "rule";
-           List
-             [
-               Atom "action";
-               List
-                 [
-                   Atom "with-outputs-to";
-                   Atom Fpath.(to_string (set_ext ".gen" target_path));
-                   List
-                     ([
-                        Atom "run";
-                        Atom "odoc";
-                        Atom (Fpath.to_string backend ^ "-targets");
-                        Atom "-o";
-                        Atom ".";
-                        Atom ("%{dep:" ^ Fpath.to_string path ^ "}");
-                      ]
-                     @ flat_flag);
-                 ];
-             ];
-         ]
-        @ enabledif);
-    ]
-
-let target_diff_rule enabledif backend path =
-  let p = Fpath.( // ) backend path in
-  List
-    ([
-       Atom "rule";
-       List [ Atom "alias"; Atom "runtest" ];
-       List
-         [
-           Atom "action";
-           List
-             [
-               Atom "diff";
-               Atom (Fpath.to_string p);
-               Atom Fpath.(to_string (p |> set_ext ".gen"));
-             ];
-         ];
-     ]
-    @ enabledif)
-
-let gen_and_diff_target_files_rules enabledif ?flat_flag backend p =
-  match flat_flag with
-  | Some flat_flag -> (
-      let path = Fpath.relativize ~root:backend p in
-      match path with
-      | Some p ->
-          let p' = create_targets_file p in
-          [
-            gen_targets_file enabledif ~flat_flag backend p' p;
-            target_diff_rule enabledif backend p';
-          ]
-      | None -> [])
-  | None -> (
-      let path = Fpath.relativize ~root:backend p in
-      match path with
-      | Some p ->
-          let p' = create_targets_file p in
-          [
-            gen_targets_file enabledif backend p' p;
-            target_diff_rule enabledif backend p';
-          ]
-      | None -> [])
-
-let gen_backend_diff_rule enabledif (b_t_r, b, _) p =
-  let p = odocls b p in
-  match expected_targets' b p with
+let gen_backend_diff_rule enabledif ~targets (b_t_r, b, _) p =
+  match targets with
   | [] -> []
   | _ ->
-      [
-        List
-          [
-            Atom "subdir";
-            Atom (Fpath.to_string b);
-            List
-              ([
-                 Atom "rule";
-                 List (Atom "targets" :: expected_targets' b p);
-                 b_t_r p;
+      let targets_gen = List.map (Fpath.add_ext ".gen") targets in
+      Dune.
+        [
+          subdir b
+            (rule ?enabledif
+               [
+                 List
+                   (Atom "targets"
+                    :: List.map (fun t -> Atom (Dune.arg_fpath t)) targets_gen);
+                 action (run (b_t_r p));
                ]
-              @ enabledif);
-          ];
-      ]
-
-let diff_rule enabledif backend t =
-  let t' = Fpath.( // ) backend t in
-  List
-    ([
-       Atom "rule";
-       List [ Atom "alias"; Atom "runtest" ];
-       List
-         [
-           Atom "action";
-           List
-             [
-               Atom "diff";
-               Atom (Fpath.to_string t');
-               Atom (Fpath.to_string t' ^ ".gen");
-             ];
-         ];
-     ]
-    @ enabledif)
-
-let diff_rule enabledif backend p =
-  let t = expected_targets backend p |> List.map Fpath.v in
-  List.map (diff_rule enabledif backend) t
+             :: List.map2 (Dune.runtest_diff ?enabledif) targets targets_gen);
+        ]
 
 let gen_backend_rule enabledif backend_target_rules path =
   List.map
     (fun b_t_r ->
-      let _, b, flag = b_t_r in
-      match flag with
-      | Some flat_flag ->
-          [
-            gen_backend_diff_rule enabledif b_t_r path;
-            diff_rule enabledif b path;
-            gen_and_diff_target_files_rules enabledif ~flat_flag b path;
-          ]
-          |> List.concat
-      | None ->
-          [
-            gen_backend_diff_rule enabledif b_t_r path;
-            diff_rule enabledif b path;
-            gen_and_diff_target_files_rules enabledif b path;
-          ]
-          |> List.concat)
+      let _, b, flat_flag = b_t_r in
+      let targets = expected_targets b path in
+      let relpath =
+        let path = Fpath.relativize ~root:b path in
+        match path with Some p -> p | None -> assert false
+      in
+      let targets_file = targets_file_path relpath in
+      [
+        gen_backend_diff_rule enabledif ~targets b_t_r relpath;
+        gen_targets_file enabledif ?flat_flag b targets_file relpath;
+      ]
+      |> List.concat)
     backend_target_rules
   |> List.flatten
 
@@ -331,9 +185,7 @@ let gen_rule backend_target_rules test_cases =
       List.(concat (map gen_rule_for_source_file test_cases));
       List.map
         (fun case ->
-          gen_backend_rule
-            (render_enabledif case.enabledif)
-            backend_target_rules case.odocl)
+          gen_backend_rule case.enabledif backend_target_rules case.odocl)
         test_cases
       |> List.flatten;
     ]
