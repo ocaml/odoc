ba72d417f70ec10f9557eb3460226290438bd8dd
Jules Aguillon <juloo.dsi@gmail.com>
2021-07-21 18:13:12 +0200

    Fix generators test handling internal files
    The generators tests were complaining about the '.formatted' directory
    created by Dune. This commit refactor the handling of input files to
    avoid this.

--- test/generators/gen_rules/gen_rules.ml
+++ test/generators/gen_rules/gen_rules.ml
@@ -1,3 +1,7 @@
+let die s =
+  prerr_endline s;
+  exit 1
+
 let html_target_rule path : Gen_rules_lib.sexp =
   List
     [
@@ -63,40 +67,70 @@ let man_target_rule path : Gen_rules_lib.sexp =
         ];
     ]
 
-let read_file_from_dir dir =
-  let filenames =
-    let arr = Sys.readdir dir in
-    Array.sort String.compare arr;
-    Array.to_list arr
-  in
-  let dir = Fpath.v dir in
-  List.map (Fpath.( / ) dir) filenames
+(** Returns filenames, not paths. *)
+let read_files_from_dir dir =
+  let arr = Sys.readdir (Fpath.to_string dir) in
+  Array.sort String.compare arr;
+  Array.to_list arr
 
 let constraints =
   let open Gen_rules_lib in
   [
-    (Fpath.v "stop_dead_link_doc.mli", Min "4.04");
-    (Fpath.v "bugs_post_406.mli", Min "4.06");
-    (Fpath.v "ocamlary.mli", Min "4.07");
-    (Fpath.v "recent.mli", Min "4.09");
-    (Fpath.v "labels.mli", Min "4.09");
-    (Fpath.v "recent_impl.ml", Min "4.09");
-    (Fpath.v "bugs_pre_410.ml", Max "4.09");
-    (Fpath.v "module_type_subst.mli", Min "4.13");
+    ("stop_dead_link_doc.mli", Min "4.04");
+    ("bugs_post_406.mli", Min "4.06");
+    ("ocamlary.mli", Min "4.07");
+    ("recent.mli", Min "4.09");
+    ("labels.mli", Min "4.09");
+    ("recent_impl.ml", Min "4.09");
+    ("bugs_pre_410.ml", Max "4.09");
+    ("module_type_subst.mli", Min "4.13");
   ]
 
-let () =
-  let paths = read_file_from_dir (Fpath.filename Gen_rules_lib.cases) in
-  let paths =
-    List.filter (fun p -> not (Gen_rules_lib.is_dot_ocamlformat p)) paths
+let test_cases_dir = Fpath.v "cases"
+
+(** Make a test cases or return the empty list if the given file should be
+    ignored. Might abort the program with an error. *)
+let make_test_case case_name =
+  let input = Fpath.(/) test_cases_dir case_name in
+  let mk odoc_prefix cmt_suffix =
+    let base_out_path = Fpath.v (odoc_prefix ^ case_name) in
+    let cmt =
+      match cmt_suffix with
+      | Some suf -> Some (Fpath.set_ext suf base_out_path)
+      | None -> None
+    in
+    let odoc = Fpath.set_ext ".odoc" base_out_path in
+    let odocl = Fpath.set_ext ".odocl" base_out_path in
+    let enabledif =
+      try Some (List.assoc case_name constraints) with Not_found -> None
+    in
+    { Gen_rules_lib.input; cmt; odoc; odocl; enabledif }
   in
+  match Fpath.get_ext input with
+  | ".ml" -> [ mk "" (Some ".cmt") ]
+  | ".mli" -> [ mk "" (Some ".cmti") ]
+  | ".mld" -> [ mk "page-" None ]
+  (* Dune creates directories starting with a dot, which result in an empty
+     extension with Fpath. Also, there's [.ocamlformat]. *)
+  | "" -> []
+  | ext ->
+      die
+        (Format.asprintf
+           "Don't know what to do with %a because of unrecognized %s extension."
+           Fpath.pp input ext)
+
+let read_test_cases () =
+  read_files_from_dir test_cases_dir |> List.map make_test_case |> List.concat
+
+let () =
+  let cases = read_test_cases () in
   let stanzas =
-    Gen_rules_lib.gen_rule constraints
+    Gen_rules_lib.gen_rule
       [
         (html_target_rule, Fpath.v "html", Some "--flat");
         (latex_target_rule, Fpath.v "latex", None);
         (man_target_rule, Fpath.v "man", None);
       ]
-      paths
+      cases
   in
   List.iter (Sexplib0.Sexp.pp Format.std_formatter) stanzas
