e0af36578f2f81580bc08def74c25152b348cb7c
Jon Ludlam <jon@recoil.org>
2021-09-15 14:24:48 +0100

    Strengthen module types
    When strengthening a module, we now 'strengthen' module types too,
    so for all module types declared in `X`, if we're strenthening with
    `Y`, we replace e.g. for module type `A` in `X`, `X.A = sig ... end`
    with `X.A = Y.A`.
    
    These 'module type aliases' are then not expanded, resulting in a
    reduction in number of output files.

--- src/xref2/strengthen.ml
+++ src/xref2/strengthen.ml
@@ -83,11 +83,7 @@ and module_ :
 and module_type :
     Cpath.module_type -> Component.ModuleType.t -> Component.ModuleType.t =
  fun prefix m ->
-  let expr =
-    match m.expr with
-    | None -> Some (ModuleType.Path { p_path = prefix; p_expansion = None })
-    | Some _ -> m.expr
-  in
+  let expr = Some (ModuleType.Path { p_path = prefix; p_expansion = None }) in
   { m with expr }
 
 and type_decl : Cpath.type_ -> TypeDecl.t -> TypeDecl.t =
