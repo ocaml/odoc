e503a0cf99284c38d32be5b2d02e30830223ba81
asavahista <91232572+asavahista@users.noreply.github.com>
2021-09-24 10:07:52 +1000

    fix line number off-by-one-error (#730)

--- src/loader/odoc_loader.ml
+++ src/loader/odoc_loader.ml
@@ -4,7 +4,7 @@ module Error = Odoc_model.Error
 let read_string parent_definition filename text =
   let location =
     let pos =
-      Lexing.{ pos_fname = filename; pos_lnum = 0; pos_cnum = 0; pos_bol = 0 }
+      Lexing.{ pos_fname = filename; pos_lnum = 1; pos_cnum = 0; pos_bol = 0 }
     in
     Location.{ loc_start = pos; loc_end = pos; loc_ghost = true }
   in
