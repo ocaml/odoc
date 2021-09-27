a8fd949a072a5badf914b7d8b5de19a053e4da6c
Jon Ludlam <jon@recoil.org>
2021-09-27 15:05:17 +0100

    Updates

--- doc/examples/markup.mli
+++ doc/examples/markup.mli
@@ -118,6 +118,34 @@ module type Foo = sig
   end
 end
 
+(** {1 Stop comments} *)
+
+module Stop : sig
+class type foo =
+  object
+    (** comment for method m *)
+    method m : string
+
+    (**/**)
+
+    (** This method won't appear in the documentation *)
+    method bar : int
+  end
+
+(** This value appears in the documentation, since the Stop special comment
+    in the class does not affect the parent module of the class.*)
+val foo : string
+
+(**/**)
+(** The value bar does not appear in the documentation.*)
+val bar : string
+(**/**)
+
+(** The type t appears since in the documentation since the previous stop comment
+toggled off the "no documentation mode". *)
+type t = string
+end
+
 (** {1 Synopsis, preambles and sections} *)
 
 (** This is the comment attached to the declaration of Hidden__Module *)
