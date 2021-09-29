0678c2ff3d5f0079f2349f0411163cf5c7119e3b
Jon Ludlam <jon@recoil.org>
2021-09-29 16:40:23 +0100

    WIP

--- doc/examples/markup.mli
+++ doc/examples/markup.mli
@@ -121,29 +121,31 @@ end
 (** {1 Stop comments} *)
 
 module Stop : sig
-class type foo =
-  object
-    (** comment for method m *)
-    method m : string
+  (** This module demonstrates the use of stop comments ([(**/**)]) *)
 
-    (**/**)
+  class type foo =
+    object
+      (** comment for method m *)
+      method m : string
 
-    (** This method won't appear in the documentation *)
-    method bar : int
-  end
+      (**/**)
 
-(** This value appears in the documentation, since the Stop special comment
-    in the class does not affect the parent module of the class.*)
-val foo : string
+      (** This method won't appear in the documentation *)
+      method bar : int
+    end
 
-(**/**)
-(** The value bar does not appear in the documentation.*)
-val bar : string
-(**/**)
+  (** This value appears in the documentation, since the Stop special comment
+      in the class does not affect the parent module of the class.*)
+  val foo : string
 
-(** The type t appears since in the documentation since the previous stop comment
-toggled off the "no documentation mode". *)
-type t = string
+  (**/**)
+  (** The value bar does not appear in the documentation.*)
+  val bar : string
+  (**/**)
+
+  (** The type t appears since in the documentation since the previous stop comment
+  toggled off the "no documentation mode". *)
+  type t = string
 end
 
 (** {1 Synopsis, preambles and sections} *)
