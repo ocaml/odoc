1bc7de59f5479d6c2c56b18de8832aa0764c4963
lubegasimon <lubegasimon73@gmail.com>
2021-10-06 12:11:44 +0300

    Add doc comments to document/types
    Signed-off-by: lubegasimon <lubegasimon73@gmail.com>

--- src/document/types.ml
+++ src/document/types.ml
@@ -5,6 +5,10 @@ module rec Class : sig
 end =
   Class
 
+(** InternalLink. 
+  
+      An internal link is one that is formed as a result of references, either
+      resolved or unresolved *)
 and InternalLink : sig
   type resolved = Url.t * Inline.t
 
@@ -25,11 +29,16 @@ and Source : sig
   type t = token list
 
   and tag = string option
+  (** any OCaml keyword, forexample, module, sig et cetera. *)
 
-  and token = Elt of Inline.t | Tag of tag * t
+  and token =
+    | Elt of Inline.t
+        (** Elt is anything part of the inline content, including the white space. *)
+    | Tag of tag * t
 end =
   Source
 
+(** Inline is for content wrapped in one line. *)
 and Inline : sig
   type entity = string
 
@@ -41,18 +50,24 @@ and Inline : sig
 
   and desc =
     | Text of string
-    | Entity of entity
+    | Entity of entity  (** special characters such as `->`. *)
     | Linebreak
     | Styled of style * t
     | Link of href * t
+        (**  A link to a given address, that is, [ {{: string} inline-text}]. *)
     | InternalLink of InternalLink.t
     | Source of Source.t
     | Raw_markup of Raw_markup.t
 end =
   Inline
 
+(** description for [odoc] documentation tags*)
 and Description : sig
-  type one = { attr : Class.t; key : Inline.t; definition : Block.t }
+  type one = {
+    attr : Class.t;  (** [odoc]-specific documentation tag. *)
+    key : Inline.t;  (** key for doc tag. *)
+    definition : Block.t;  (** the value for a doc tag. *)
+  }
 
   type t = one list
 end =
@@ -63,14 +78,21 @@ and Heading : sig
 end =
   Heading
 
+(** Block is for multi-inline content. *)
 and Block : sig
   type t = one list
 
-  and one = { attr : Class.t; desc : desc }
+  and one = {
+    attr : Class.t;
+        (** forexample `at-tags` for
+        @<{{:https://ocaml.github.io/odoc/odoc_for_authors.html#tags}odoc-specific>}
+        documentation tags. *)
+    desc : desc;
+  }
 
   and desc =
-    | Inline of Inline.t
-    | Paragraph of Inline.t
+    | Inline of Inline.t  (** text/data in a single line. *)
+    | Paragraph of Inline.t  (** multi line text. *)
     | List of list_type * t list
     | Description of Description.t
     | Source of Source.t
@@ -81,6 +103,7 @@ and Block : sig
 end =
   Block
 
+(** DocumentedSrc is for any OCaml syntax that has documentation attached to it. *)
 and DocumentedSrc : sig
   type 'a documented = {
     attrs : Class.t;
@@ -101,18 +124,27 @@ and DocumentedSrc : sig
 end =
   DocumentedSrc
 
+(** Alternative is for OCaml syntaxes that are expandable, but as well
+    representable in summary form. *)
 and Alternative : sig
   type expansion = {
     status : [ `Inline | `Open | `Closed | `Default ];
     summary : Source.t;
+        (** summary of a module, that is, [module X : sig ... end]. *)
     expansion : DocumentedSrc.t;
-    url : Url.Path.t;
+        (** expandable form of a module, for example {[
+          module Foo X : sig
+            type t
+          end
+        ]} *)
+    url : Url.Path.t;  (** resolved/unresolved reference of module. *)
   }
 
   type t = Expansion of expansion
 end =
   Alternative
 
+(**  Subpage is the lower level of a module. *)
 and Subpage : sig
   type status = [ `Inline | `Open | `Closed | `Default ]
 
@@ -132,7 +164,7 @@ and Item : sig
     attr : Class.t;
     anchor : Url.Anchor.t option;
     content : 'a;
-    doc : Block.t;
+    doc : Block.t;  (** doc comment. *)
   }
 
   type declaration = DocumentedSrc.t item
@@ -149,9 +181,12 @@ end =
 
 and Page : sig
   type t = {
-    title : string;
+    title : string;  (** The filename. *)
     header : Item.t list;
+        (** Is the `Module + filename`, however, for subpages, it is
+      `kind of the subpage + filename`. for example `Class + filename`. *)
     items : Item.t list;
+        (** other contents of the page, forexample, body, footer etc. *)
     url : Url.Path.t;
   }
 end =
