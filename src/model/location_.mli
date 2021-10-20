23bf80a4dc85b72f994b683e14cf6edd49134df9
Jules Aguillon <juloo.dsi@gmail.com>
2021-10-20 12:53:29 +0200

    Add Location_.pp

--- src/model/location_.mli
+++ src/model/location_.mli
@@ -6,6 +6,8 @@ val set_end_as_offset_from_start : int -> span -> span
 
 val in_string : string -> offset:int -> length:int -> span -> span
 
+val pp : Format.formatter -> span -> unit
+
 val pp_span_start : Format.formatter -> span -> unit
 
 val span_equal : span -> span -> bool
