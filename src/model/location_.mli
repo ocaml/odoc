d3a275c1da162808786b69e8cf14214bd9823e33
Jules Aguillon <juloo.dsi@gmail.com>
2021-11-04 19:48:36 +0100

    Fix ambiguous label declaration warning
    Make sure the warning triggers if identifiers are conflicting, not just
    label names.

--- src/model/location_.mli
+++ src/model/location_.mli
@@ -7,3 +7,5 @@ val set_end_as_offset_from_start : int -> span -> span
 val in_string : string -> offset:int -> length:int -> span -> span
 
 val pp_span_start : Format.formatter -> span -> unit
+
+val span_equal : span -> span -> bool
