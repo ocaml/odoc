21571b6914655768365959ef77f0ee4f2aa03758
Jon Ludlam <jon@recoil.org>
2021-11-23 18:25:13 +0000

    Move a broken bit of Ocamlary into a test

--- src/ocamlary/ocamlary.mli
+++ src/ocamlary/ocamlary.mli
@@ -886,17 +886,17 @@ module CanonicalTest : sig
     val foo : int L.t -> float L.t
 
     val bar : 'a List.t -> 'a List.t
-    (** This is just {!List.id}, or rather {!L.id} *)
+    (* This is just {!List.id}, or rather {!L.id} *)
 
     val baz : 'a Base__.List.t -> unit
-    (** We can't reference [Base__] because it's hidden.
+    (* We can't reference [Base__] because it's hidden.
         {!List.t} ([List.t]) should resolve. *)
   end
 
   module List_modif : module type of Base.List with type 'c t = 'c Base__.List.t
 end
 
-val test : 'a CanonicalTest.Base__.List.t -> unit
+(* val test : 'a CanonicalTest.Base__.List.t -> unit *)
 (** Some ref to {!CanonicalTest.Base_Tests.C.t} and {!CanonicalTest.Base_Tests.L.id}.
     But also to {!CanonicalTest.Base.List} and {!CanonicalTest.Base.List.t} *)
 
