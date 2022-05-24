df926d1bcaa470d1fbea4f09a288dc2c5daedecc
Jon Ludlam <jon@recoil.org>
2022-05-24 11:57:58 +0100

    Add debugging to Env when there are missing idents being removed
    This should never happen, but it does. Further bug fixing required.

--- src/xref2/env.ml
+++ src/xref2/env.ml
@@ -119,14 +119,23 @@ end = struct
   let remove id t =
     let id = (id :> Identifier.t) in
     let name = Identifier.name id in
-    let l = StringMap.find name t in
+    let l =
+      try StringMap.find name t
+      with e ->
+        Format.eprintf "Failed to find %s\n%!" name;
+        raise e
+    in
     match
       List.filter
         (fun e ->
           not (Identifier.equal id (Component.Element.identifier e.elem)))
         l
     with
-    | [] -> StringMap.remove name t
+    | [] -> (
+        try StringMap.remove name t
+        with Not_found ->
+          Format.eprintf "Failed to find %s\n%!" name;
+          raise Not_found)
     | xs -> StringMap.add name xs (StringMap.remove name t)
 
   let find_by_name f name t =
