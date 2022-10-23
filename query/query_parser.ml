dd100931270a72f450d414e13528650256e48fb4
ArthurW <arthur@tarides.com>
2022-10-23 14:40:01 +0200

    guess type search

--- query/query_parser.ml
+++ query/query_parser.ml
@@ -11,10 +11,16 @@ let alphanum = function
 let naive_of_string str =
   List.filter (fun s -> String.length s > 0) (String.split_on_char ' ' str)
 
+let guess_type_search str =
+  String.length str >= 1
+  && (str.[0] = '\'' || String.contains str '-' || String.contains str '(')
+
 let of_string str =
+  let str = String.trim str in
   let ok, str_name, str_typ =
     match String.split_on_char ':' str with
     | [ a; b ] -> true, a, b
+    | _ when guess_type_search str -> true, "", str
     | _ -> false, str, ""
   in
   let pretty, ps =
