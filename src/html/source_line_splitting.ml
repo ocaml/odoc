f002d4182cf18b10c4259e4c67b71c8fa311510d
Paul-Elliot <peada@free.fr>
2022-12-01 11:30:53 +0100

    fix line-splitting breaking syntax highlighting
    Some OCaml token can be spread in several lines (for instance, multilines
    comments), so breaking into lines could break the tree structure of the set of
    positions. This is fixed by making lines be just anchors to the beginning of the
    line.
    
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- src/html/source_line_splitting.ml
+++ src/html/source_line_splitting.ml
@@ -8,7 +8,7 @@ let lines_locs src =
     List.fold_left
       (fun (i, poses, count) line ->
         let l = String.length line in
-        let poses = (i, (count, count + l)) :: poses in
+        let poses = (i, (count, count)) :: poses in
         (i + 1, poses, count + l + 1))
       (1, [], 0) lines
   in
