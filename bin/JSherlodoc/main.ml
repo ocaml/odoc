246b6f0f2b098702698f29c03f3a5f122b2d70db
ArthurW <arthur@tarides.com>
2023-04-23 13:57:45 +0200

    style js results

--- bin/JSherlodoc/main.ml
+++ bin/JSherlodoc/main.ml
@@ -5,33 +5,37 @@ open Brr
 let inner_html = El.Prop.jstr (Jstr.v "innerHTML")
 
 let raw_html str =
-  let elt = El.div [] in
+  let elt = El.div ~at:At.[ class' (Jstr.of_string "docstring") ] [] in
   El.set_prop inner_html (Jstr.v str) elt ;
   elt
 
 let latest = ref 0
-let count = ref 0
+let count = ref 1
 
-let search ~id input _event =
+let search ~id input =
   let query = El.prop El.Prop.value input |> Jstr.to_string in
-  let pretty_query, results =
+  let _pretty_query, results =
     Query.(api ~shards:db { query; packages = []; limit = 50 })
   in
+  let results = List.of_seq @@ Seq.take 10 @@ List.to_seq results in
   let results =
-    El.[ p [ txt' pretty_query ] ]
-    @ List.map
-        (fun elt ->
-          El.(
-            div
-              ([ p
-                   ~at:At.[ style (Jstr.of_string "color:red") ]
-                   [ txt' elt.Db.Elt.name ]
-               ]
-              @
-              match elt.Db.Elt.doc with
-              | None -> []
-              | Some doc -> [ raw_html doc ])))
-        results
+    List.map
+      (fun elt ->
+        El.(
+          div
+            ~at:At.[ class' (Jstr.of_string "result") ]
+            ([ code
+                 [ txt' "val "
+                 ; em [ txt' elt.Db.Elt.name ]
+                 ; txt' " : "
+                 ; txt' elt.Db.Elt.str_type
+                 ]
+             ]
+            @
+            match elt.Db.Elt.doc with
+            | None -> []
+            | Some doc -> [ raw_html doc ])))
+      results
   in
   let results_div =
     Document.find_el_by_id G.document (Jstr.of_string "results") |> Option.get
@@ -42,17 +46,21 @@ let search ~id input _event =
     El.set_children results_div results
   end
 
-let search input event =
+let search input =
   let id = !count in
   count := id + 1 ;
-  search ~id input event
+  search ~id input
 
 let main () =
   let search_input =
     Document.find_el_by_id G.document (Jstr.of_string "search") |> Option.get
   in
-  Ev.(
-    listen input (search search_input)
-      (search_input |> El.document |> Document.as_target))
+  let _ =
+    Ev.(
+      listen input
+        (fun _ -> search search_input)
+        (search_input |> El.document |> Document.as_target))
+  in
+  search search_input
 
 let _ = main ()
