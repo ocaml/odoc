a9465dcb288974cb3ab072473a592c0dd8137332
Emile Trotignon <emile@tarides.com>
2023-06-28 16:56:47 +0200

    Bugfix in Succ and make it polymorphic

--- query/succ.ml
+++ query/succ.ml
@@ -1,17 +1,40 @@
-open Db
-
-type s =
+type 'a s =
   | All
   | Empty
-  | Array of Elt.t array
-  | Inter of s * s
-  | Union of s * s
+  | Array of 'a array
+  | Inter of 'a s * 'a s
+  | Union of 'a s * 'a s
 
-type t =
+type 'a t =
   { cardinal : int
-  ; s : s
+  ; s : 'a s
   }
 
+let rec print_s a ~depth s =
+  print_string (String.make (depth * 4) ' ') ;
+  let depth = depth + 1 in
+  match s with
+  | All -> print_endline "All"
+  | Empty -> print_endline "Empty"
+  | Inter (l, r) ->
+      print_endline "Inter" ;
+      print_s a ~depth l ;
+      print_s a ~depth r
+  | Union (l, r) ->
+      print_endline "Union" ;
+      print_s a ~depth l ;
+      print_s a ~depth r
+  | Array arr ->
+      print_string "{ " ;
+      Array.iter
+        (fun elt ->
+          a elt ;
+          print_string " ")
+        arr ;
+      print_endline "}"
+
+let print_s a s = print_s a ~depth:0 s
+let print a t = print_s a t.s
 let all = { cardinal = -1; s = All }
 let empty = { cardinal = 0; s = Empty }
 
@@ -55,25 +78,20 @@ let union_of_array arr =
 
 let union_of_list li = li |> Array.of_list |> union_of_array
 
-let best x y =
-  match Elt.compare x y with
+let best ~compare x y =
+  match compare x y with
   | 0 -> x
   | c when c < 0 -> x
   | _ -> y
 
-let update_candidate old_cand new_cand =
-  Some
-    (match old_cand with
-    | Some old_cand -> best old_cand new_cand
-    | None -> new_cand)
-
-let best_opt old_cand new_cand =
+let best_opt ~compare old_cand new_cand =
   match old_cand, new_cand with
   | None, None -> None
   | None, Some z | Some z, None -> Some z
-  | Some x, Some y -> Some (best x y)
+  | Some x, Some y -> Some (best ~compare x y)
 
 let ( let* ) = Option.bind
+let ( let+ ) x f = Option.map f x
 
 type strictness =
   | Gt
@@ -84,56 +102,54 @@ let array_succ ~strictness =
   | Ge -> Array_succ.succ_ge
   | Gt -> Array_succ.succ_gt
 
-let rec succ ~strictness t elt =
+let rec succ ~compare ~strictness t elt =
   (* Printf.printf "depth : %i\n" depth ; *)
   match t with
   | All -> invalid_arg "Succ.succ_rec All"
   | Empty -> None
-  | Array arr -> array_succ ~strictness ~compare:Elt.compare elt arr
+  | Array arr -> array_succ ~strictness ~compare elt arr
   | Union (l, r) ->
-      let elt_r = succ ~strictness r elt in
-      let elt_l = succ ~strictness l elt in
-      best_opt elt_l elt_r
+      let elt_r = succ ~compare ~strictness r elt in
+      let elt_l = succ ~compare ~strictness l elt in
+      best_opt ~compare elt_l elt_r
   | Inter (l, r) ->
       let rec loop elt_r =
-        let* elt_l = succ ~strictness l elt_r in
-        let* elt_r = succ ~strictness:Ge r elt_l in
-        if Elt.equal elt_l elt_r then Some elt_l else loop elt_r
+        let* elt_l = succ ~compare ~strictness l elt_r in
+        let* elt_r = succ ~compare ~strictness:Ge r elt_l in
+        if compare elt_l elt_r = 0 then Some elt_l else loop elt_r
       in
       loop elt
 
-let succ_ge = succ ~strictness:Ge
-let succ_gt = succ ~strictness:Gt
+let succ_ge ~compare t elt = succ ~compare ~strictness:Ge t elt
+let succ_gt ~compare t elt = succ ~compare ~strictness:Gt t elt
 
-let rec first candidate t =
+let rec first ~compare candidate t =
   match t with
   | All -> invalid_arg "Succ.first All"
   | Empty -> None
-  | Array s -> ( try update_candidate candidate s.(0) with e -> raise e)
+  | Array s -> best_opt ~compare candidate (Some s.(0))
   | Inter (a, _) ->
-      let* elt = first candidate a in
-      succ_ge t elt
+      let* elt = first ~compare candidate a in
+      succ_ge ~compare t elt
   | Union (a, b) -> begin
-      let a = first candidate a in
-      let candidate = best_opt candidate a in
-      first candidate b
+      let candidate = first ~compare candidate a in
+      first ~compare candidate b
     end
 
-let first = first None
+let first ~compare t = first ~compare None t
 
-let first_exn t =
-  match first t with
+let first_exn ~compare t =
+  match first ~compare t with
   | Some v -> v
   | None -> raise Not_found
 
-let to_seq t =
-  (* PPrint.ToChannel.pretty 0.8 80 stdout (pprint t) ; *)
+let to_seq ~compare t =
   let state = ref None in
   let loop () =
     let elt =
       match !state with
-      | None -> first t
-      | Some previous_elt -> succ_gt t previous_elt
+      | None -> first ~compare t
+      | Some previous_elt -> succ_gt ~compare t previous_elt
     in
     state := elt ;
     elt
