70f1645ae8dd918b17ff3fa030356214e6eb5f6b
Guillaume Petiot <guillaume@tarides.com>
2023-08-04 16:19:00 +0100

    Bigger example
    Co-authored-by: Paul-Elliot <peada@free.fr>

--- test/xref2/github_issue_932.t/foo.mli
+++ test/xref2/github_issue_932.t/foo.mli
@@ -6,7 +6,27 @@ type t +=
 | A
 | B
 
-(** - {!t}
-    - {!extension-decl-A}
-    - {!extension-A}
-    - {!extension-B} *)
+module M : sig
+  type t = ..
+
+  type t +=
+  | A
+  | B
+end
+
+type M.t += C
+
+(** - t : {!t}
+    - extension-decl-A : {!extension-decl-A}
+    - extension-decl-B : {!extension-decl-B}
+    - extension-A : {!extension-A}
+    - extension-B : {!extension-B}
+    - A : {!A}
+
+    - M.t : {!M.t}
+    - M.extension-decl-A : {!M.extension-decl-A}
+    - M.extension-decl-B : {!M.extension-decl-B}
+    - M.extension-A : {!M.extension-A}
+    - M.extension-B : {!M.extension-B}
+    - M.A : {!M.A}
+ *)
