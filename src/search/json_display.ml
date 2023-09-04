769c76ad4d5c6499d39bce8a10623172348a6cee
Emile Trotignon <emile@tarides.com>
2023-09-04 11:39:02 +0200

    remove extra div from json display

--- src/search/json_display.ml
+++ src/search/json_display.ml
@@ -1,7 +1,8 @@
-
-let of_entry { Entry.id; doc = _; kind = _ } h :
-    Odoc_html.Json.json =
+let of_entry { Entry.id; doc = _; kind = _ } h : Odoc_html.Json.json =
   let url = Html.url id in
-  let html = Tyxml.Html.(div ~a:[ a_class [ "search-entry" ] ] h) in
-  let html = Format.asprintf "%a" (Tyxml.Html.pp_elt ()) html in
+  let html =
+    h
+    |> List.map (fun html -> Format.asprintf "%a" (Tyxml.Html.pp_elt ()) html)
+    |> String.concat "\n"
+  in
   `Object [ ("url", `String url); ("html", `String html) ]
