7f3926ed3fd481428252beb9b1a2e96dce5401e2
Paul-Elliot <peada@free.fr>
2023-09-13 15:34:47 +0200

    compile-index: take a file containing a list of file, as input
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- src/odoc/indexing.ml
+++ src/odoc/indexing.ml
@@ -1,31 +1,29 @@
+open Astring
 open Odoc_json_index
 open Or_error
-
-let map_result f = function Ok v -> Ok (f v) | Error _ as e -> e
+open Odoc_model
 
 let handle_file file ~unit ~page =
-  Odoc_file.load file
-  |> map_result @@ fun unit' ->
-     match unit' with
-     | { Odoc_file.content = Unit_content unit'; _ } when not unit'.hidden ->
-         Some (unit unit')
-     | { Odoc_file.content = Page_content page'; _ } -> Some (page page')
-     | _ -> None
+  Odoc_file.load file >>= fun unit' ->
+  match unit' with
+  | { Odoc_file.content = Unit_content unit'; _ } when not unit'.hidden ->
+      Ok (unit unit')
+  | { Odoc_file.content = Page_content page'; _ } -> Ok (page page')
+  | _ ->
+      Error
+        (`Msg
+          "Only pages and unit are allowed as input when generating an index")
 
-let fold_dirs ~dirs ~unit ~page ~init =
-  dirs
-  |> List.fold_left
-       (fun acc dir ->
-         acc >>= fun acc ->
-         Fs.Directory.fold_files_rec_result ~ext:"odocl"
-           (fun acc file ->
-             file |> handle_file ~unit:(unit acc) ~page:(page acc) >>= function
-             | None -> Ok acc
-             | Some acc -> Ok acc)
-           acc dir)
-       (Ok init)
+let parse_input_file input =
+  let is_sep = function '\n' | '\r' -> true | _ -> false in
+  Fs.File.read input >>= fun content ->
+  let files =
+    String.fields ~empty:false ~is_sep content |> List.rev_map Fs.File.of_string
+  in
+  Ok files
 
-let compile ~output dirs =
+let compile ~output ~warnings_options input =
+  parse_input_file input >>= fun files ->
   let output_channel =
     Fs.Directory.mkdir_p (Fs.File.dirname output);
     open_out_bin (Fs.File.to_string output)
@@ -37,8 +35,23 @@ let compile ~output dirs =
     false
   in
   Format.fprintf output "[";
-  fold_dirs ~dirs ~unit:(print Json_search.unit) ~page:(print Json_search.page)
-    ~init:true
-  >>= fun _ ->
+  let index () =
+    List.fold_left
+      (fun acc file ->
+        match
+          handle_file
+            ~unit:(print Json_search.unit acc)
+            ~page:(print Json_search.page acc)
+            file
+        with
+        | Ok acc -> acc
+        | Error (`Msg m) ->
+            Error.raise_warning ~non_fatal:true
+              (Error.filename_only "%S" m (Fs.File.to_string input));
+            acc)
+      true files
+  in
+  let result = Error.catch_warnings index in
+  result |> Error.handle_warnings ~warnings_options >>= fun (_ : bool) ->
   Format.fprintf output "]";
   Ok ()
