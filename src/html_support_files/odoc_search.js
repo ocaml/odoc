31d5da5dac29670884673f7d74da3389bd295f80
Paul-Elliot <peada@free.fr>
2023-09-14 14:35:43 +0200

    Search: improve search UI
    Adds a "loader" circle when search is performed, and a "no results"
    entry when there is no results.
    
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- src/html_support_files/odoc_search.js
+++ src/html_support_files/odoc_search.js
@@ -18,16 +18,30 @@ function createWebWorker() {
 }
 
 var worker;
+var waiting = 0;
+
+function wait() {
+  waiting = waiting + 1;
+  document.querySelector(".search-snake").classList.add("search-busy");
+}
+
+function stop_waiting() {
+  if (waiting > 0) waiting = waiting - 1;
+  else waiting = 0;
+  if (waiting == 0) {
+    document.querySelector(".search-snake").classList.remove("search-busy");
+  }
+}
 
 document.querySelector(".search-bar").addEventListener("focus", (ev) => {
   if (typeof worker == "undefined") {
     worker = createWebWorker();
     worker.onmessage = (e) => {
+      stop_waiting();
       let results = e.data;
       let search_results = document.querySelector(".search-result-inner");
       search_results.innerHTML = "";
       let f = (entry) => {
-        /* entry */
         let search_result = document.createElement("a");
         search_result.classList.add("search-entry");
         search_result.href = base_url + entry.url;
@@ -35,10 +49,18 @@ document.querySelector(".search-bar").addEventListener("focus", (ev) => {
         search_results.appendChild(search_result);
       };
       results.forEach(f);
+      let search_request = document.querySelector(".search-bar").value;
+      if (results.length == 0 && search_request != "") {
+        let no_result = document.createElement("div");
+        no_result.classList.add("search-no-result");
+        no_result.innerText = "No result...";
+        search_results.appendChild(no_result);
+      }
     };
   }
 });
 
 document.querySelector(".search-bar").addEventListener("input", (ev) => {
+  wait();
   worker.postMessage(ev.target.value);
 });
