cafb6134975cd855e099950c327f5f730c028068
Emile Trotignon <emile@tarides.com>
2023-12-04 16:42:50 +0100

    whole switch test fix

--- test/whole_switch/test.sh
+++ test/whole_switch/test.sh
@@ -4,5 +4,5 @@ cd packages
 for PKG in $(ls $OPAM_SWITCH_PREFIX/var/cache/odig/odoc)
 do
     echo $PKG
-    dune exec sherlodoc_index -- --format=marshal --db=$PKG.db $(find /home/emile/.opam/sherlodoc-test2/var/cache/odig/odoc/$PKG -name "*.odocl") 2> $PKG.stderr > $PKG.stdout
+    dune exec sherlodoc_index -- --format=marshal --db=$PKG.db $(find $OPAM_SWITCH_PREFIX/var/cache/odig/odoc/$PKG -name "*.odocl") 2> $PKG.stderr > $PKG.stdout
 done
