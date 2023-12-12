831dc84554a785c843f871f27192d8334ffdf16f
Paul-Elliot <peada@free.fr>
2023-12-12 09:17:30 +0100

    Revert "Adding datatype and constructor to lang model"
    This reverts commit 5826eb1985d5bd1f5288058de27df202479eca25.

--- src/model/paths.mli
+++ src/model/paths.mli
@@ -182,12 +182,6 @@ module Identifier : sig
     module Type :
       IdSig with type t = Id.path_type and type t_pv = Id.path_type_pv
 
-    module DataType :
-      IdSig with type t = Id.path_datatype and type t_pv = Id.path_datatype_pv
-
-    module Constructor :
-      IdSig with type t = Id.path_constructor and type t_pv = Id.constructor_pv
-
     module Value : IdSig with type t = Id.path_value and type t_pv = Id.value_pv
 
     module ClassType :
@@ -372,14 +366,6 @@ module rec Path : sig
       (* val identifier : t -> Identifier.Path.Type.t *)
     end
 
-    module DataType : sig
-      type t = Paths_types.Resolved_path.datatype
-    end
-
-    module Constructor : sig
-      type t = Paths_types.Resolved_path.constructor
-    end
-
     module Value : sig
       type t = Paths_types.Resolved_path.value
     end
@@ -411,14 +397,6 @@ module rec Path : sig
 
   module Type : sig
     type t = Paths_types.Path.type_
-  end
-
-  module DataType : sig
-    type t = Paths_types.Path.datatype
-  end
-
-  module Constructor : sig
-    type t = Paths_types.Path.constructor
   end
 
   module Value : sig
