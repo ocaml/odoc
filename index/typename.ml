cdfb34da59d55a9fe2ac0cee2d723107ba93bdc2
ArthurW <arthur@tarides.com>
2023-12-21 18:29:13 +0100

    simplify index typename

--- index/typename.ml
+++ index/typename.ml
@@ -1,43 +1,31 @@
-(* This might be hard to maintain but it is useful *)
-
-open Odoc_model
+module Path = Odoc_model.Paths.Path
+module Identifier = Odoc_model.Paths.Identifier
+module TypeName = Odoc_model.Names.TypeName
 module ModuleName = Odoc_model.Names.ModuleName
-module H = Tyxml.Html
 
-let show_module_name h md =
-  Format.fprintf h "%s" (Odoc_model.Names.ModuleName.to_string md)
-
-let rec show_ident_long h (r : Paths.Identifier.t_pv Paths.Identifier.id) =
-  match r.Paths.Identifier.iv with
-  | `CoreType n -> Format.fprintf h "Stdlib.%s" (Names.TypeName.to_string n)
-  | `Type (md, n) ->
-    Format.fprintf h "%a.%s" show_signature md (Names.TypeName.to_string n)
-  | _ -> Format.fprintf h "%S" (r |> Paths.Identifier.fullname |> String.concat ".")
-
-and show_module_t h p =
-  Format.fprintf
-    h
-    "%s"
-    (Odoc_document.Url.render_path (p : Paths.Path.Module.t :> Paths.Path.t))
+let rec show_ident_long h (r : Identifier.t_pv Identifier.id) =
+  match r.iv with
+  | `CoreType n -> Format.fprintf h "Stdlib.%s" (TypeName.to_string n)
+  | `Type (md, n) -> Format.fprintf h "%a.%s" show_signature md (TypeName.to_string n)
+  | _ -> Format.fprintf h "%S" (r |> Identifier.fullname |> String.concat ".")
 
 and show_signature h sig_ =
   match sig_.iv with
-  | `Root (_, name) -> Format.fprintf h "%s" (Odoc_model.Names.ModuleName.to_string name)
-  | `Module (pt, mdl) -> Format.fprintf h "%a.%a" show_signature pt show_module_name mdl
-  | `Parameter (_, p) -> Format.fprintf h "%s" (Odoc_model.Names.ModuleName.to_string p)
+  | `Root (_, name) -> Format.fprintf h "%s" (ModuleName.to_string name)
+  | `Module (pt, mdl) ->
+    Format.fprintf h "%a.%s" show_signature pt (ModuleName.to_string mdl)
+  | `Parameter (_, p) -> Format.fprintf h "%s" (ModuleName.to_string p)
   | `Result t -> Format.fprintf h "%a" show_signature t
   | `ModuleType (_, p) ->
     Format.fprintf h "%s" (Odoc_model.Names.ModuleTypeName.to_string p)
 
-let show_type_name_verbose h : Paths.Path.Type.t -> _ = function
+let show_type_name_verbose h : Path.Type.t -> _ = function
   | `Resolved t ->
-    let open Paths.Path in
-    Format.fprintf h "%a" show_ident_long (Resolved.identifier (t :> Resolved.t))
+    Format.fprintf h "%a" show_ident_long Path.Resolved.(identifier (t :> t))
   | `Identifier (path, _hidden) ->
-    let name =
-      (path :> Paths.Identifier.t) |> Paths.Identifier.fullname |> String.concat "."
-    in
+    let name = String.concat "." @@ Identifier.fullname path in
     Format.fprintf h "%s" name
-  | `Dot (mdl, x) -> Format.fprintf h "%a.%s" show_module_t mdl x
+  | `Dot (mdl, x) ->
+    Format.fprintf h "%s.%s" (Odoc_document.Url.render_path (mdl :> Path.t)) x
 
 let to_string t = Format.asprintf "%a" show_type_name_verbose t
