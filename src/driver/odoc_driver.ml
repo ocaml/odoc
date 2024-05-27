8bda797f38015f395c1cb86f274c5e38aa330630
Paul-Elliot <peada@free.fr>
2024-05-27 14:53:18 +0200

    Odoc driver: review comment
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- src/driver/odoc_driver.ml
+++ src/driver/odoc_driver.ml
@@ -509,7 +509,7 @@ let run libs verbose odoc_dir html_dir stats nb_workers =
       (fun () ->
         let compiled = Compile.compile odoc_dir all in
         let linked = Compile.link compiled in
-        let _ = Compile.html_generate html_dir linked in
+        let () = Compile.html_generate html_dir linked in
         let _ = Odoc.support_files html_dir in
         ())
       (fun () -> render_stats env nb_workers)
