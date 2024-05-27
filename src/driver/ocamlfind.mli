0c0806ae35704e96f0d7540a903f49d80c4ebf4b
Paul-Elliot <peada@free.fr>
2024-05-27 14:51:08 +0200

    TODO: is it package or library?
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- src/driver/ocamlfind.mli
+++ src/driver/ocamlfind.mli
@@ -5,7 +5,7 @@ val archives : string -> string list
 (** Returns the list of archive file names of a given library *)
 
 val sub_libraries : string -> Util.StringSet.t
-(** Returns the list of sublibraries of a given package *)
+(** Returns the list of sublibraries of a given library *)
 
 val deps : string list -> (string list, [> `Msg of string ]) result
-(** Returns the list of transitive package dependencies of given packages *)
+(** Returns the list of transitive package dependencies of given libraries *)
