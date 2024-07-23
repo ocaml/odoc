58254c81ebf8c49c9cd98d33752a5e52b56e15ca
Paul-Elliot <peada@free.fr>
2024-07-23 14:10:19 +0200

    Driver: improve compile mli
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- src/driver/compile.mli
+++ src/driver/compile.mli
@@ -3,11 +3,16 @@ type compiled
 val init_stats : Packages.set -> unit
 
 val compile :
-  Fpath.t option ->
+  ?partial:Fpath.t ->
   output_dir:Fpath.t ->
   ?linked_dir:Fpath.t ->
   Packages.set ->
   compiled list
+(** Use [partial] to reuse the output of a previous call to [compile]. Useful in
+    the voodoo context.
+
+    [output_dir] is the directory for [odoc] file, [linked_dir] is the one for
+    [odocl] files (defaulting to [output_dir] when absent). *)
 
 type linked
 
