356fc49fd64c8ffd0c67fa810dd1f26a7a55e9e9
Paul-Elliot <peada@free.fr>
2024-08-20 14:39:12 +0200

    Remove generation of implementation using html-generate

--- src/odoc/rendering.ml
+++ src/odoc/rendering.ml
@@ -2,67 +2,52 @@ open Odoc_document
 open Or_error
 open Odoc_model
 
-let check_empty_source_arg source filename =
-  if source <> None then
-    Error.raise_warning
-    @@ Error.filename_only
-         "--source and --source-root only have an effect when generating from \
-          an implementation"
-         filename
-
-let documents_of_unit ~warnings_options ~syntax ~source ~renderer ~extra
-    ~filename unit =
+let documents_of_unit ~warnings_options ~syntax ~renderer ~extra unit =
   Error.catch_warnings (fun () ->
-      check_empty_source_arg source filename;
       renderer.Renderer.extra_documents extra (CU unit))
   |> Error.handle_warnings ~warnings_options
   >>= fun extra_docs ->
   Ok (Renderer.document_of_compilation_unit ~syntax unit :: extra_docs)
 
-let documents_of_page ~warnings_options ~syntax ~source ~renderer ~extra
-    ~filename page =
+let documents_of_page ~warnings_options ~syntax ~renderer ~extra page =
   Error.catch_warnings (fun () ->
-      check_empty_source_arg source filename;
       renderer.Renderer.extra_documents extra (Page page))
   |> Error.handle_warnings ~warnings_options
   >>= fun extra_docs -> Ok (Renderer.document_of_page ~syntax page :: extra_docs)
 
-let documents_of_implementation ~warnings_options:_ ~syntax impl source =
-  match (source, impl.Lang.Implementation.id) with
-  | Some source_file, Some _ -> (
-      match Fs.File.read source_file with
-      | Error (`Msg msg) ->
-          Error (`Msg (Format.sprintf "Couldn't load source file: %s" msg))
-      | Ok source_code ->
-          let syntax_info =
-            Syntax_highlighter.syntax_highlighting_locs source_code
-          in
-          let rendered =
-            Odoc_document.Renderer.documents_of_implementation ~syntax impl
-              syntax_info source_code
-          in
-          Ok rendered)
-  | _, None ->
-      Error (`Msg "The implementation unit was not compiled with --source-id.")
-  | None, _ ->
-      Error
-        (`Msg
-          "--source or --source-root should be passed when generating \
-           documents for an implementation.")
+(* let documents_of_implementation ~warnings_options:_ ~syntax impl source = *)
+(*   match (source, impl.Lang.Implementation.id) with *)
+(*   | Some source_file, Some _ -> ( *)
+(*       match Fs.File.read source_file with *)
+(*       | Error (`Msg msg) -> *)
+(*           Error (`Msg (Format.sprintf "Couldn't load source file: %s" msg)) *)
+(*       | Ok source_code -> *)
+(*           let syntax_info = *)
+(*             Syntax_highlighter.syntax_highlighting_locs source_code *)
+(*           in *)
+(*           let rendered = *)
+(*             Odoc_document.Renderer.documents_of_implementation ~syntax impl *)
+(*               syntax_info source_code *)
+(*           in *)
+(*           Ok rendered) *)
+(*   | _, None -> *)
+(*       Error (`Msg "The implementation unit was not compiled with --source-id.") *)
+(*   | None, _ -> *)
+(*       Error *)
+(*         (`Msg *)
+(* "--source or --source-root should be passed when generating \ *)
+   (*            documents for an implementation.") *)
 
-let documents_of_odocl ~warnings_options ~renderer ~extra ~source ~syntax input
-    =
+let documents_of_odocl ~warnings_options ~renderer ~extra ~syntax input =
   Odoc_file.load input >>= fun unit ->
-  let filename = Fpath.to_string input in
   match unit.content with
   | Odoc_file.Page_content odoctree ->
-      documents_of_page ~warnings_options ~syntax ~source ~renderer ~extra
-        ~filename odoctree
-  | Impl_content impl ->
-      documents_of_implementation ~warnings_options ~syntax impl source
+      documents_of_page ~warnings_options ~syntax ~renderer ~extra odoctree
+  | Impl_content _impl ->
+      (* documents_of_implementation ~warnings_options ~syntax impl source *)
+      failwith "TODO ERROR"
   | Unit_content odoctree ->
-      documents_of_unit ~warnings_options ~source ~syntax ~renderer ~extra
-        ~filename odoctree
+      documents_of_unit ~warnings_options ~syntax ~renderer ~extra odoctree
   | Asset_content _ -> Ok [] (* TODO *)
 
 let documents_of_input ~renderer ~extra ~resolver ~warnings_options ~syntax
@@ -70,10 +55,10 @@ let documents_of_input ~renderer ~extra ~resolver ~warnings_options ~syntax
   let output = Fs.File.(set_ext ".odocl" input) in
   Odoc_link.from_odoc ~resolver ~warnings_options input output >>= function
   | `Page page -> Ok [ Renderer.document_of_page ~syntax page ]
-  | `Impl impl -> Ok (Renderer.documents_of_implementation ~syntax impl [] "")
-  | `Module m ->
-      documents_of_unit ~warnings_options ~source:None ~filename:"" ~syntax
-        ~renderer ~extra m
+  | `Impl _impl ->
+      (* Ok (Renderer.documents_of_implementation ~syntax impl [] "") *)
+      failwith "TODO ERROR"
+  | `Module m -> documents_of_unit ~warnings_options ~syntax ~renderer ~extra m
   | `Asset _ -> Ok [] (* TODO *)
 
 let render_document renderer ~sidebar ~output:root_dir ~extra_suffix ~extra doc
@@ -115,15 +100,14 @@ let render_odoc ~resolver ~warnings_options ~syntax ~renderer ~output extra file
   Ok ()
 
 let generate_odoc ~syntax ~warnings_options ~renderer ~output ~extra_suffix
-    ~source_file ~sidebar extra file =
+    ~sidebar extra file =
   (match sidebar with
   | None -> Ok None
   | Some x ->
       Odoc_file.load_index x >>= fun (sidebar, _) ->
       Ok (Some (Odoc_document.Sidebar.of_lang sidebar)))
   >>= fun sidebar ->
-  documents_of_odocl ~warnings_options ~renderer ~source:source_file ~extra
-    ~syntax file
+  documents_of_odocl ~warnings_options ~renderer ~extra ~syntax file
   >>= fun docs ->
   List.iter
     (render_document renderer ~output ~sidebar ~extra_suffix ~extra)
@@ -131,14 +115,12 @@ let generate_odoc ~syntax ~warnings_options ~renderer ~output ~extra_suffix
   Ok ()
 
 let targets_odoc ~resolver ~warnings_options ~syntax ~renderer ~output:root_dir
-    ~extra ~source_file odoctree =
+    ~extra odoctree =
   let docs =
     if Fpath.get_ext odoctree = ".odoc" then
       documents_of_input ~renderer ~extra ~resolver ~warnings_options ~syntax
         odoctree
-    else
-      documents_of_odocl ~warnings_options ~renderer ~extra ~syntax
-        ~source:source_file odoctree
+    else documents_of_odocl ~warnings_options ~renderer ~extra ~syntax odoctree
   in
   docs >>= fun docs ->
   List.iter
