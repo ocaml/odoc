ff09f077f55042a99d59f3f3052a6b00f7db5929
Jon Ludlam <jon@recoil.org>
2024-08-23 16:31:50 +0100

    driver: Extract library dependencies from META files

--- src/driver/library_names.mli
+++ src/driver/library_names.mli
@@ -1,4 +1,4 @@
-type library = { name : string; archive_name : string; dir : string option }
+type library = { name : string; archive_name : string; dir : string option; deps : string list }
 
 val process_meta_file : Fpath.t -> library list
 (** From a path to a [Meta] file, returns the list of libraries defined in this
