fe6aac45bdf87a54050266adf55f6f9ef245e4fa
Jon Ludlam <jon@recoil.org>
2024-08-28 16:40:58 +0100

    Driver: Capture stderr when executing commands
    Log it when the command has failed.

--- src/driver/run.ml
+++ src/driver/run.ml
@@ -22,10 +22,13 @@ type executed_command = {
    the produced file. *)
 let commands = ref []
 
+let n = Atomic.make 0
+
 (** Return the list of executed commands where the first argument was [cmd]. *)
 let run env cmd output_file =
   let cmd = Bos.Cmd.to_list cmd in
-  Logs.debug (fun m -> m "Executing: %s" (String.concat " " cmd));
+  let myn = Atomic.fetch_and_add n 1 in
+  Logs.debug (fun m -> m "%d - Executing: %s" myn (String.concat " " cmd));
   let proc_mgr = Eio.Stdenv.process_mgr env in
   let t_start = Unix.gettimeofday () in
   let env =
@@ -38,18 +41,45 @@ let run env cmd output_file =
       env []
     |> Array.of_list
   in
-  let stderr = Buffer.create 1024 in
-  let err = Eio.Flow.buffer_sink stderr in
   (* Logs.debug (fun m -> m "Running cmd %a" Fmt.(list ~sep:sp string) cmd); *)
   let r =
-    Eio.Process.parse_out proc_mgr Eio.Buf_read.take_all ~env ~stderr:err cmd
+    Eio.Switch.run ~name:"Process.parse_out" @@ fun sw ->
+    let r, w = Eio.Process.pipe proc_mgr ~sw in
+    let re, we = Eio.Process.pipe proc_mgr ~sw in
+    try
+      let child =
+        Eio.Process.spawn ~sw proc_mgr ~stdout:w ~stderr:we ~env cmd
+      in
+      Eio.Flow.close w;
+      Eio.Flow.close we;
+      let output, err =
+        Eio.Fiber.pair
+          (fun () ->
+            Eio.Buf_read.parse_exn Eio.Buf_read.take_all r ~max_size:max_int)
+          (fun () ->
+            Eio.Buf_read.parse_exn Eio.Buf_read.take_all re ~max_size:max_int)
+      in
+      Eio.Flow.close r;
+      Eio.Flow.close re;
+      match Eio.Process.await child with
+      | `Exited 0 -> output
+      | `Exited n ->
+          Logs.err (fun m -> m "%d - Process exitted %d: stderr=%s" myn n err);
+          failwith "Error"
+      | `Signaled n ->
+          Logs.err (fun m -> m "%d - Signalled %d: stderr=%s" myn n err);
+          failwith ("Signaled " ^ string_of_int n)
+    with Eio.Exn.Io _ as ex ->
+      let bt = Printexc.get_raw_backtrace () in
+      Eio.Exn.reraise_with_context ex bt "%d - running command: %a" myn
+        Eio.Process.pp_args cmd
   in
   (* Logs.debug (fun m ->
       m "Finished running cmd %a" Fmt.(list ~sep:sp string) cmd); *)
   let t_end = Unix.gettimeofday () in
   let r = String.split_on_char '\n' r in
   let time = t_end -. t_start in
-  let errors = Buffer.contents stderr in
+  let errors = "" in
   commands := { cmd; time; output_file; errors } :: !commands;
   r
 
