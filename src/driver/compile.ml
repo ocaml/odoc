a10df96f2054d7d7abade759aceed5af4cee7f72
Jon Ludlam <jon@recoil.org>
2024-09-16 18:47:18 +0100

    Driver: Allow multiple copies of an interface
    This is for where partitioning of modules into libraries isn't so
    straightforward. A good example of this is 'ocamlmiddleend' which
    contains many of the same modules as 'ocamloptcomp'. Rather than
    trying to assign a module to one or other of these libraries, instead
    we're allowing duplication, and so the docs for that module will
    appear in _both_ libraries.

--- src/driver/compile.ml
+++ src/driver/compile.ml
@@ -8,7 +8,11 @@ let mk_byhash (pkgs : Odoc_unit.t list) =
   List.fold_left
     (fun acc (u : Odoc_unit.t) ->
       match u.Odoc_unit.kind with
-      | `Intf { hash; _ } as kind -> Util.StringMap.add hash { u with kind } acc
+      | `Intf { hash; _ } as kind ->
+          let elt = { u with kind } in
+          Util.StringMap.update hash
+            (function None -> Some [ elt ] | Some x -> Some (elt :: x))
+            acc
       | _ -> acc)
     Util.StringMap.empty pkgs
 
@@ -50,8 +54,8 @@ let init_stats (units : Odoc_unit.t list) =
 open Eio.Std
 
 type partial =
-  (string * Odoc_unit.intf Odoc_unit.unit) list
-  * Odoc_unit.intf Odoc_unit.unit Util.StringMap.t
+  (string * Odoc_unit.intf Odoc_unit.unit list) list
+  * Odoc_unit.intf Odoc_unit.unit list Util.StringMap.t
 
 let unmarshal filename : partial =
   let ic = open_in_bin (Fpath.to_string filename) in
@@ -66,8 +70,8 @@ let marshal (v : partial) filename =
     ~finally:(fun () -> close_out oc)
     (fun () -> Marshal.to_channel oc v [])
 
-let find_partials odoc_dir : Odoc_unit.intf Odoc_unit.unit Util.StringMap.t * _
-    =
+let find_partials odoc_dir :
+    Odoc_unit.intf Odoc_unit.unit list Util.StringMap.t * _ =
   let tbl = Hashtbl.create 1000 in
   let hashes_result =
     OS.Dir.fold_contents ~dotfiles:false ~elements:`Dirs
@@ -100,42 +104,47 @@ let compile ?partial ~partial_dir ?linked_dir:_ (all : Odoc_unit.t list) =
       | None -> (Util.StringMap.empty, Hashtbl.create 10)
     in
     let all_hashes =
-      Util.StringMap.union (fun _x o1 _o2 -> Some o1) hashes other_hashes
+      Util.StringMap.union (fun _x o1 o2 -> Some (o1 @ o2)) hashes other_hashes
     in
     let compile_one compile_other hash =
       match Util.StringMap.find_opt hash all_hashes with
       | None ->
           Logs.debug (fun m -> m "Error locating hash: %s" hash);
           Error Not_found
-      | Some unit ->
-          let deps = match unit.kind with `Intf { deps; _ } -> deps in
-          let _fibers =
-            Fiber.List.map
-              (fun (other_unit : Odoc_unit.intf Odoc_unit.unit) ->
-                match compile_other other_unit with
-                | Ok r -> Some r
-                | Error _exn ->
-                    Logs.debug (fun m ->
-                        m
-                          "Error during compilation of module %s (hash %s, \
-                           required by %s)"
-                          (Fpath.filename other_unit.input_file)
-                          (match other_unit.kind with
-                          | `Intf { hash; _ } -> hash)
-                          (Fpath.filename unit.input_file));
-                    None)
-              deps
-          in
-          let includes = unit.include_dirs in
-          Odoc.compile ~output_dir:unit.output_dir ~input_file:unit.input_file
-            ~includes ~parent_id:unit.parent_id;
-          Atomic.incr Stats.stats.compiled_units;
+      | Some units ->
+          Ok
+            (List.map
+               (fun (unit : Odoc_unit.intf Odoc_unit.unit) ->
+                 let deps = match unit.kind with `Intf { deps; _ } -> deps in
+                 let _fibers =
+                   Fiber.List.map
+                     (fun (other_unit : Odoc_unit.intf Odoc_unit.unit) ->
+                       match compile_other other_unit with
+                       | Ok r -> Some r
+                       | Error _exn ->
+                           Logs.debug (fun m ->
+                               m
+                                 "Error during compilation of module %s (hash \
+                                  %s, required by %s)"
+                                 (Fpath.filename other_unit.input_file)
+                                 (match other_unit.kind with
+                                 | `Intf { hash; _ } -> hash)
+                                 (Fpath.filename unit.input_file));
+                           None)
+                     deps
+                 in
+                 let includes = unit.include_dirs in
+                 Odoc.compile ~output_dir:unit.output_dir
+                   ~input_file:unit.input_file ~includes
+                   ~parent_id:unit.parent_id;
+                 Atomic.incr Stats.stats.compiled_units;
 
-          Ok unit
+                 unit)
+               units)
     in
     let rec compile_mod :
         Odoc_unit.intf Odoc_unit.unit ->
-        (Odoc_unit.intf Odoc_unit.unit, exn) Result.t =
+        (Odoc_unit.intf Odoc_unit.unit list, exn) Result.t =
      fun unit ->
       let hash = match unit.kind with `Intf { hash; _ } -> hash in
       match Hashtbl.find_opt tbl hash with
@@ -153,7 +162,7 @@ let compile ?partial ~partial_dir ?linked_dir:_ (all : Odoc_unit.t list) =
   let compile (unit : Odoc_unit.t) =
     match unit.kind with
     | `Intf _ as kind ->
-        (compile_mod { unit with kind } :> (Odoc_unit.t, _) Result.t)
+        (compile_mod { unit with kind } :> (Odoc_unit.t list, _) Result.t)
     | `Impl src ->
         let includes = unit.include_dirs in
         let source_id = src.src_id in
@@ -161,25 +170,35 @@ let compile ?partial ~partial_dir ?linked_dir:_ (all : Odoc_unit.t list) =
           ~input_file:unit.input_file ~includes ~parent_id:unit.parent_id
           ~source_id;
         Atomic.incr Stats.stats.compiled_impls;
-        Ok unit
+        Ok [ unit ]
     | `Asset ->
         Odoc.compile_asset ~output_dir:unit.output_dir ~parent_id:unit.parent_id
           ~name:(Fpath.filename unit.input_file);
         Atomic.incr Stats.stats.compiled_assets;
-        Ok unit
+        Ok [ unit ]
     | `Mld ->
         let includes = unit.include_dirs in
         Odoc.compile ~output_dir:unit.output_dir ~input_file:unit.input_file
           ~includes ~parent_id:unit.parent_id;
         Atomic.incr Stats.stats.compiled_mlds;
-        Ok unit
+        Ok [ unit ]
   in
   let res = Fiber.List.map compile all in
   (* For voodoo mode, we need to keep which modules successfully compiled *)
   let zipped =
     List.filter_map
       (function
-        | Ok (Odoc_unit.{ kind = `Intf { hash; _ }; _ } as b) -> Some (hash, b)
+        | Ok (Odoc_unit.{ kind = `Intf { hash; _ }; _ } :: _ as b) ->
+            let l =
+              List.filter_map
+                (function
+                  | Odoc_unit.{ kind = `Intf { hash = hash'; _ }; _ } as x
+                    when hash' = hash ->
+                      Some x
+                  | _ -> None)
+                b
+            in
+            Some (hash, l)
         | _ -> None)
       res
   in
