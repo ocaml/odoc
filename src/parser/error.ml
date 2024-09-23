117c78b5218bf133e6468816d816333886142111
faycarsons <faycarsons23@gmail.com>
2024-09-23 12:53:08 -0400

    Remove unused `Error` module

--- src/parser/error.ml
+++
@@ -1,9 +0,0 @@
-type parser_error = 
-  | Unclosed of 
-    { opening : string 
-    ; items : string 
-    ; closing : string 
-    }
-  | Expecting of string
-
-exception Parser_error of parser_error Loc.with_location 
