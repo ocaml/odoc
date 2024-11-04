fd8d89865ea82a1cf401334e6a8c0e0d5a57415c
Guillaume Petiot <guillaume@tarides.com>
2024-11-04 20:17:50 +0000

    Move Sidebar types into Odoc_index and rename Toc.of_lang into Toc_of_page_hierarchy
    Co-authored-by: Paul-Elliot <peada@free.fr>

--- src/index/odoc_index.ml
+++ src/index/odoc_index.ml
@@ -1,9 +1,17 @@
 module Entry = Entry
 module Fold = Fold
 module Page_hierarchy = Page_hierarchy
-module Sidebar = Sidebar
+
+type page = { p_name : string; p_hierarchy : Page_hierarchy.t }
+
+type library = {
+  name : string;
+  units : Odoc_model.Paths.Identifier.RootModule.t list;
+}
+
+type sidebar = { pages : page list; libs : library list }
 
 type 'a t = {
-  sidebar : Sidebar.t;
+  sidebar : sidebar;
   index : 'a Odoc_model.Paths.Identifier.Hashtbl.Any.t;
 }
--- src/index/sidebar.mli
+++
@@ -1,8 +0,0 @@
-open Odoc_model
-open Paths.Identifier
-
-type library = { name : string; units : RootModule.t list }
-
-type page_hierarchy = { hierarchy_name : string; pages : Page_hierarchy.t }
-
-type t = { pages : page_hierarchy list; libraries : library list }
