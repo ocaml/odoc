16c9e74167623d5b238f6df63b3aa2786d92f850
Guillaume Petiot <guillaume@tarides.com>
2024-11-04 17:18:53 +0000

    Move Sidebar.Toc into Page_hierarchy
    Co-authored-by: Paul-Elliot <peada@free.fr>

--- src/index/sidebar.mli
+++ src/index/sidebar.mli
@@ -1,20 +1,8 @@
 open Odoc_model
 open Paths.Identifier
 
-module PageToc : sig
-  type title = Comment.link_content
-
-  type index = Page.t * title
-  type t = index option Odoc_utils.Tree.t
-
-  val of_list :
-    (LeafPage.t * title * Frontmatter.children_order option) list -> t
-  (** Uses the convention that the [index] children passes its payload to the
-      container directory to output a payload *)
-end
-
 type library = { name : string; units : RootModule.t list }
 
-type page_hierarchy = { hierarchy_name : string; pages : PageToc.t }
+type page_hierarchy = { hierarchy_name : string; pages : Page_hierarchy.t }
 
 type t = { pages : page_hierarchy list; libraries : library list }
