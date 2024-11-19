47c0e6415ac051a412ab4ab11210dfbedade8bbd
Jon Ludlam <jon@recoil.org>
2024-11-19 11:12:53 +0000

    Don't produce a package list page in voodoo mode

--- src/driver/odoc_units_of.mli
+++ src/driver/odoc_units_of.mli
@@ -1,4 +1,8 @@
 open Odoc_unit
 
 val packages :
-  dirs:dirs -> extra_paths:Voodoo.extra_paths -> Packages.t list -> t list
+  dirs:dirs ->
+  extra_paths:Voodoo.extra_paths ->
+  gen_indices:bool ->
+  Packages.t list ->
+  t list
