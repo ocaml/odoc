82209efc082f8fa17b2845f97a92cf655bfa9a7c
Marek Kubica <marek@tarides.com>
2026-05-22 14:40:29 +0200

    Guard for `OXCAML`

--- src/loader/doc_attr.mli
+++ src/loader/doc_attr.mli
@@ -93,5 +93,7 @@ type parsed_attribute =
   ]
 
 val parse_attribute : Parsetree.attribute -> parsed_attribute option
+#if defined OXCAML
 val lang_value_attr_of_zero_alloc : Zero_alloc.t -> Lang.Value.attr option
+#endif
 val attrs_of_value_description : Types.value_description -> Lang.Value.attr list
