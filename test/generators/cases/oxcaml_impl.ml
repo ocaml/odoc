d66569edd6e29388d0038bbf2e9db185c7855ddb
Marek Kubica <marek@tarides.com>
2026-05-26 15:31:29 +0200

    Add (failing) test case for missing component code

--- test/generators/cases/oxcaml_impl.ml
+++ test/generators/cases/oxcaml_impl.ml
@@ -1,1 +1,10 @@
 let[@zero_alloc] add b x y = if b then x + y else x
+
+module To_be_included = struct
+  let[@zero_alloc] add b x y = if b then x + y else x
+  (* [add] has a zero alloc annotation that it shouldn't loose *)
+end
+
+module Including = struct
+  include To_be_included
+end
--- test/generators/markdown/Oxcaml_impl.md
+++ test/generators/markdown/Oxcaml_impl.md
@@ -4,3 +4,9 @@
 ```ocaml
 val add : bool -> int -> int -> int [@@zero_alloc arity 3]
 ```
+```ocaml
+module To_be_included : sig ... end
+```
+```ocaml
+module Including : sig ... end
+```
